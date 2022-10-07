105759449e399a086a27369d84a3c762f7ce9d25
Martin-Belton-gov <martin.belton@digital.education.gov.uk>
2022-10-07 13:18:21 +0100

    Added Client Connect

--- src/FamilyHubs.ReferralUi.Ui/Program.cs
+++ src/FamilyHubs.ReferralUi.Ui/Program.cs
@@ -1,6 +1,7 @@
 using FamilyHubs.ReferralUi.Ui.Extensions;
 using FamilyHubs.ReferralUi.Ui.Models;
 using MassTransit;
+using System.IdentityModel.Tokens.Jwt;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -26,6 +27,34 @@ if (builder.Configuration.GetValue<bool>("UseRabbitMQ"))
                         }));
 }
 
+JwtSecurityTokenHandler.DefaultMapInboundClaims = false;
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultScheme = "Cookies";
+    options.DefaultChallengeScheme = "oidc";
+})
+    .AddCookie("Cookies")
+    .AddOpenIdConnect("oidc", options =>
+    {
+        options.Authority = "https://localhost:5001";
+
+        options.ClientId = "referral";
+        options.ClientSecret = "secret";
+        options.ResponseType = "code";
+
+        options.SaveTokens = true;
+
+        options.Scope.Clear();
+        options.Scope.Add("openid");
+        options.Scope.Add("profile");
+        options.Scope.Add("offline_access");
+        options.Scope.Add("servicedirectoryapi");
+        options.Scope.Add("referralapi");
+
+        options.GetClaimsFromUserInfoEndpoint = true;
+    });
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -41,9 +70,11 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
-app.MapRazorPages();
+//app.MapRazorPages();
+app.MapRazorPages().RequireAuthorization();
 
 Program.ServiceProvider = app.Services;
 
