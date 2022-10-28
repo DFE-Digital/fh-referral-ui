30991b6469c8fb8c7177344b6525d202e17aff88
AliShah2 <kishwer01@gmail.com>
2022-10-28 13:29:18 +0100

    Enabled filtering by cost in local offer search results

---
+++ localofferresults.feature
@@ -0,0 +1,6 @@
+Feature: LocalOfferResults Page Tests
+
+   Scenario: LocalOfferResults page has a Apply filters button
+     Given a user has arrived on the LocalOfferResults page
+     When the user click on the Apply filters option
+     Then the page directed to itself
--- tests/FamilyHubs.ReferralUi.UnitTests/Pages/ProfessionalReferral/LocalOfferResultsPage.cs
+++ tests/FamilyHubs.ReferralUi.UnitTests/Pages/ProfessionalReferral/LocalOfferResultsPage.cs
@@ -1,7 +1,10 @@
-﻿using FamilyHubs.ReferralUi.Ui.Pages.ProfessionalReferral;
+﻿using FamilyHubs.ReferralUi.Ui.Pages;
+using FamilyHubs.ReferralUi.Ui.Pages.ProfessionalReferral;
 using FamilyHubs.ReferralUi.Ui.Services.Api;
+using FamilyHubs.ServiceDirectory.Shared.Models.Api.OpenReferralServices;
 using FluentAssertions;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Moq;
 using System;
 using System.Collections.Generic;
@@ -9,6 +12,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xunit;
+using Newtonsoft.Json;
 
 namespace FamilyHubs.ReferralUi.UnitTests.Pages.ProfessionalReferral;
 
@@ -23,19 +27,57 @@ public class LocalOfferResultsPage
         pageModel = new LocalOfferResultsModel(mockLocalOfferClientService.Object, mockIPostcodeLocationClientService.Object);
     }
 
+    //[Fact]
+    //public void OnGetAsync_PopulatesThePageModel_WithAListOfServices()
+    //{
+    //    throw new NotImplementedException();
+    //}
+
     [Theory]
     [InlineData(null)]
     [InlineData("")]
-    public void NoResultsShouldBeReturned_WhenSearchPostCodeIsNullOrEmpty(string postCode)
+    public void OnGetAsync_WhenSearchPostCodeIsNullOrEmpty_ThenNoResultsShouldBeReturned(string postCode)
     {
-        //Arrange
-        pageModel.SearchPostCode = postCode;
-
         // Act
-        var searchResults = pageModel.SearchResults;
+        var searchResults = pageModel.OnGetAsync(postCode, 0.0D, 0.0D, 0.0D, "", "", "") as IActionResult;
 
         // Assert
         searchResults.Should().BeNull();
     }
+
+    //[Theory]
+    //[InlineData("KT22 8RX")]
+    //public void OnGetAsync_WhenSearchPostCodeIsValid_ThenResultsShouldBeReturned(string postCode)
+    //{
+    //    // Act
+    //    var searchResults = pageModel.OnGetAsync(postCode, 0.0D, 0.0D, 0.0D, "", "", "") as IActionResult;
+
+    //    // Assert
+    //    searchResults.Should().NotBeNull();
+    //}
+
+    //[Fact]
+    //public void OnPostAsync_WhenSearchingForFreeServices_ThenFreeServicesShouldBeReturned()
+    //{
+    //    throw new NotImplementedException();
+    //}
+
+    //[Fact]
+    //public void OnPostAsync_WhenSearchingForFreeServices_ThenPaidServicesShouldNotBeReturned()
+    //{
+    //    throw new NotImplementedException();
+    //}
+
+    //[Fact]
+    //public void OnPostAsync_WhenSearchingForPaidServices_ThenPaidServicesShouldBeReturned()
+    //{
+    //    throw new NotImplementedException();
+    //}
+
+    //[Fact]
+    //public void OnPostAsync_WhenSearchingForPaidServices_ThenFreeServicesShouldNotBeReturned()
+    //{
+    //    throw new NotImplementedException();
+    //}
 
 }
