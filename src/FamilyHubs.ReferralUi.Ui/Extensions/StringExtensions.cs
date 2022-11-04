772e573ebabfe39494d94be6913057c58bc6f0b0
alidfe <113431816+alidfe@users.noreply.github.com>
2022-11-04 16:33:34 +0000

    filter search results by delivery method (#18)
    Co-authored-by: AliShah2 <kishwer01@gmail.com>

---
+++ src/FamilyHubs.ReferralUi.Ui/Extensions/StringExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace FamilyHubs.ReferralUi.Ui.Extensions;
+
+public static class StringExtensions
+{
+    public static string ToSentenceCase(this string input)
+    {
+        if (input.Length < 1)
+            return input;
+
+        string sentence = input.ToLower();
+        return sentence[0].ToString().ToUpper() + sentence.Substring(1);
+    }
+}
