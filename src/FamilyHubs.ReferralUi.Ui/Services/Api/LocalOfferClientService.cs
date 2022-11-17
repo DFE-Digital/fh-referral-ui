67e3055df64944b5e53fba4a4f063f85cd4166c0
Martin-Belton-gov <martin.belton@digital.education.gov.uk>
2022-11-17 13:31:22 +0000

    Upgrade FamilyHubs.ServiceDirectory.Shared to latest version

--- src/FamilyHubs.ReferralUi.Ui/Services/Api/LocalOfferClientService.cs
+++ src/FamilyHubs.ReferralUi.Ui/Services/Api/LocalOfferClientService.cs
@@ -6,7 +6,7 @@ namespace FamilyHubs.ReferralUi.Ui.Services.Api;
 
 public interface ILocalOfferClientService
 {
-    Task<PaginatedList<OpenReferralServiceDto>> GetLocalOffers(string status, int? minimum_age, int? maximum_age, int? given_age, string? districtCode, double? latitude, double? longtitude, double? proximity, int pageNumber, int pageSize, string text, string? serviceDeliveries, bool? isPaidFor, string? taxonmyIds, string? languages, bool? canFamilyChooseLocation);
+    Task<PaginatedList<OpenReferralServiceDto>> GetLocalOffers(string serviceType, string status, int? minimum_age, int? maximum_age, int? given_age, string? districtCode, double? latitude, double? longtitude, double? proximity, int pageNumber, int pageSize, string text, string? serviceDeliveries, bool? isPaidFor, string? taxonmyIds, string? languages, bool? canFamilyChooseLocation);
     Task<OpenReferralServiceDto> GetLocalOfferById(string id);
     //Task<PaginatedList<TestItem>> GetTestCommand(double latitude, double logtitude, double meters);
 
@@ -21,16 +21,16 @@ public class LocalOfferClientService : ApiService, ILocalOfferClientService
 
     }
 
-    public async Task<PaginatedList<OpenReferralServiceDto>> GetLocalOffers(string status, int? minimum_age, int? maximum_age, int? given_age, string? districtCode, double? latitude, double? longtitude, double? proximity, int pageNumber, int pageSize, string text, string? serviceDeliveries, bool? isPaidFor, string? taxonmyIds, string? languages, bool? canFamilyChooseLocation)
+    public async Task<PaginatedList<OpenReferralServiceDto>> GetLocalOffers(string? serviceType, string status, int? minimum_age, int? maximum_age, int? given_age, string? districtCode, double? latitude, double? longtitude, double? proximity, int pageNumber, int pageSize, string text, string? serviceDeliveries, bool? isPaidFor, string? taxonmyIds, string? languages, bool? canFamilyChooseLocation)
     {
         if (string.IsNullOrEmpty(status))
             status = "active";
 
         string url = string.Empty;
         if (latitude != null && longtitude != null && proximity != null)
-            url = $"api/services?status={status}&latitude={latitude}&longtitude={longtitude}&proximity={proximity}&pageNumber={pageNumber}&pageSize={pageSize}&text={text}";
+            url = $"api/services?serviceType={serviceType}&status={status}&latitude={latitude}&longtitude={longtitude}&proximity={proximity}&pageNumber={pageNumber}&pageSize={pageSize}&text={text}";
         else
-            url = $"api/services?status={status}&pageNumber={pageNumber}&pageSize={pageSize}&text={text}";
+            url = $"api/services?serviceType={serviceType}&status={status}&pageNumber={pageNumber}&pageSize={pageSize}&text={text}";
 
         if (minimum_age != null)
         {
