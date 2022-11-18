9063231c32fc63b0df58e2db9cdf69dab1bd337f
AliShah2 <kishwer01@gmail.com>
2022-11-18 13:02:40 +0000

    Added Redis cache

---
+++ src/FamilyHubs.ReferralUi.Ui/Services/IRedisCacheService.cs
@@ -0,0 +1,15 @@
+﻿using FamilyHubs.ServiceDirectory.Shared.Models.Api.OpenReferralServices;
+
+namespace FamilyHubs.ReferralUi.Ui.Services;
+
+public interface IRedisCacheService
+{
+    //Service
+    public OpenReferralServiceDto? RetrieveService();
+    public void StoreService(OpenReferralServiceDto serviceDto);
+
+    //Navigation - last page name
+    public string RetrieveLastPageName();
+    public void StoreCurrentPageName(string? currPage);
+    public void ResetLastPageName();
+}
---
+++ src/FamilyHubs.ReferralUi.Ui/Services/RedisCacheService.cs
@@ -0,0 +1,48 @@
+﻿using FamilyHubs.ServiceDirectory.Shared.Helpers;
+using FamilyHubs.ServiceDirectory.Shared.Models.Api.OpenReferralServices;
+using FamilyHubs.ReferralUi.Ui.Models;
+using static FamilyHubs.ReferralUi.Ui.Infrastructure.Configuration.TempStorageConfiguration;
+
+
+namespace FamilyHubs.ReferralUi.Ui.Services;
+
+public class RedisCacheService : IRedisCacheService
+{
+    private readonly IRedisCache _redisCache;
+
+    public RedisCacheService(IRedisCache redisCache)
+    {
+        _redisCache = redisCache;
+    }
+
+    public void ResetOrganisationWithService()
+    {
+        _redisCache.SetStringValue(KeyOrgWithService, String.Empty);
+    }
+
+    public string RetrieveLastPageName()
+    {
+        return _redisCache.GetStringValue(KeyCurrentPage) ?? string.Empty;
+    }
+
+    public void StoreCurrentPageName(string? currPage)
+    {
+        if (currPage != null)
+            _redisCache.SetStringValue(KeyCurrentPage, currPage);
+    }
+
+    public OpenReferralServiceDto? RetrieveService()
+    {
+        return _redisCache.GetValue<OpenReferralServiceDto>(KeyService);
+    }
+
+    public void StoreService(OpenReferralServiceDto serviceDto)
+    {
+        _redisCache.SetValue<OpenReferralServiceDto>(KeyService, serviceDto);
+    }
+
+    public void ResetLastPageName()
+    {
+        _redisCache.SetStringValue(KeyCurrentPage, String.Empty);
+    }
+}
