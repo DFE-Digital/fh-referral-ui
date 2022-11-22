0830d453cafead58379e9d22ea46ebc476b653ab
Martin-Belton-gov <martin.belton@digital.education.gov.uk>
2022-11-22 08:00:23 +0000

    Removed unwanted code

--- tests/FamilyHubs.ReferralUi.UnitTests/Pages/ProfessionalReferral/LocalOfferResultsPage.cs
+++ tests/FamilyHubs.ReferralUi.UnitTests/Pages/ProfessionalReferral/LocalOfferResultsPage.cs
@@ -27,12 +27,6 @@ public class LocalOfferResultsPage
         pageModel = new LocalOfferResultsModel(mockLocalOfferClientService.Object, mockIPostcodeLocationClientService.Object);
     }
 
-    //[Fact]
-    //public void OnGetAsync_PopulatesThePageModel_WithAListOfServices()
-    //{
-    //    throw new NotImplementedException();
-    //}
-
     [Theory]
     [InlineData(null)]
     [InlineData("")]
@@ -44,40 +38,4 @@ public class LocalOfferResultsPage
         // Assert
         searchResults.Should().BeNull();
     }
-
-    //[Theory]
-    //[InlineData("KT22 8RX")]
-    //public void OnGetAsync_WhenSearchPostCodeIsValid_ThenResultsShouldBeReturned(string postCode)
-    //{
-    //    // Act
-    //    var searchResults = pageModel.OnGetAsync(postCode, 0.0D, 0.0D, 0.0D, "", "", "") as IActionResult;
-
-    //    // Assert
-    //    searchResults.Should().NotBeNull();
-    //}
-
-    //[Fact]
-    //public void OnPostAsync_WhenSearchingForFreeServices_ThenFreeServicesShouldBeReturned()
-    //{
-    //    throw new NotImplementedException();
-    //}
-
-    //[Fact]
-    //public void OnPostAsync_WhenSearchingForFreeServices_ThenPaidServicesShouldNotBeReturned()
-    //{
-    //    throw new NotImplementedException();
-    //}
-
-    //[Fact]
-    //public void OnPostAsync_WhenSearchingForPaidServices_ThenPaidServicesShouldBeReturned()
-    //{
-    //    throw new NotImplementedException();
-    //}
-
-    //[Fact]
-    //public void OnPostAsync_WhenSearchingForPaidServices_ThenFreeServicesShouldNotBeReturned()
-    //{
-    //    throw new NotImplementedException();
-    //}
-
 }
