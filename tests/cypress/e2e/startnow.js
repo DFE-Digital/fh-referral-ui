01b7c168ced9a651dc649641a7704c388587fc1d
alidfe <113431816+alidfe@users.noreply.github.com>
2022-12-13 13:10:40 +0000

    Modified start page header and added cypress tests (#70)
    Co-authored-by: AliShah2 <kishwer01@gmail.com>

--- tests/cypress/e2e/startnow.js
+++ tests/cypress/e2e/startnow.js
@@ -16,7 +16,14 @@ When("the user click on the start now option", () => {
     cy.get('[data-testid="button-start"]').click();
 });
 
-Then("the page directed to sign into your account page", () => {
-    cy.location('pathname').should('match', new RegExp("/ProfessionalReferral/SignIn"));
+Then("the page directed to search page", () => {
+    cy.location('pathname').should('match', new RegExp("/ProfessionalReferral/Search"));
 });
 
+When("the user click on the child abuse link", () => {
+    cy.get('[data-testid="child-abuse-link"]').click();
+});
+
+Then("the page directed to child abuse url", () => {
+    cy.url().should('eq', 'https://www.gov.uk/report-child-abuse')
+});
