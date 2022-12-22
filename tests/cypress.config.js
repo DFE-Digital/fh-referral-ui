34840bbf12dc4f28a2eddf172a585465ac6e37b4
VReddy <vipin.reddy@digital.education.gov.uk>
2022-12-22 15:03:19 +0000

    Added reports to cypress configuration

--- tests/cypress.config.js
+++ tests/cypress.config.js
@@ -66,4 +66,21 @@ module.exports = defineConfig({
         specPattern: "**/*.feature",
         setupNodeEvents,
     },
+    reporter: 'cypress-multi-reporters',
+    reporterOptions: {
+        reporterEnabled: 'cypress-mochawesome-reporter, mocha-junit-reporter',
+        cypressMochawesomeReporterReporterOptions: {
+            reportDir: 'cypress/reports',
+            charts: true,
+            reportPageTitle: 'My Test Suite',
+            embeddedScreenshots: true,
+            inlineAssets: true
+        },
+        mochaJunitReporterReporterOptions: {
+            mochaFile: 'cypress/reports/junit/results-[hash].xml'
+        }
+    },
+    video: false
+
 });
+
