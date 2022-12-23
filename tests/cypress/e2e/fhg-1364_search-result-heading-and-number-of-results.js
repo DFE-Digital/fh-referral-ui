772d12a2dc17b2e9070641bdf5e549c364488e9f
Anton Palitsyn <110397000+palitsyndfe@users.noreply.github.com>
2022-12-23 09:41:38 +0000

    fhg1364-fhg1395 - verifying search results titles and removing and clearing filters functionality. Original PRs are to be closed due to conflicts. (#89)

---
+++ tests/cypress/e2e/fhg-1364_search-result-heading-and-number-of-results.js
@@ -0,0 +1,26 @@
+﻿import { Given, Then, When } from "@badeball/cypress-cucumber-preprocessor";
+
+Given("a user has arrived on search page", () => {
+    cy.visit(`ProfessionalReferral/Search`);
+});
+
+When("user search with a valid postcode {string}", (postcode) => {
+    cy.get('[data-testid="postcode-value"]').type(postcode);
+    cy.get('[data-testid="button-search"]').click();
+});
+
+Then("the user should see search results", () => {
+    cy.get('[data-testid="aidforchildrenwithtracheostomies"]').should("exist");
+});
+
+Then("the user should see {string} title", (title) => {
+    cy.get('[data-testid="search-results-title"]').should("contain.text", title);
+});
+
+Then("the user should see {string} message", (results) => {
+    cy.get('[data-testid="number-of-results"]').should("contain.text", results);
+});
+
+Then("the user should see {string} poscode", (postcode) => {
+    cy.get('[data-testid="postcodelink"]').should("contain.text", postcode);
+});
---
+++ tests/cypress/e2e/fhg1395_updatedfilterscloud.js
@@ -0,0 +1,52 @@
+﻿import { Given, Then, When } from "@badeball/cypress-cucumber-preprocessor";
+
+Given("a user has arrived on search page", () => {
+    cy.visit(`ProfessionalReferral/Search`);
+});
+
+When("user search with a valid postcode {string}", (postcode) => {
+    cy.get('[data-testid="postcode-value"]').type(postcode);
+    cy.get('[data-testid="button-search"]').click();
+});
+
+Then("the user should see search results", () => {
+    cy.get('[data-testid="aidforchildrenwithtracheostomies"]').should("exist");
+});
+
+When("use applies Free & In Person filters", () => {
+    cy.get('[data-testid="checkbox-free"]').click();
+    cy.get('[data-testid="checkbox-inperson"]').click();
+    cy.get('[data-testid="button-apply-filters"]').click();
+});
+
+Then("the filter cloud should contain these filters", () => {
+    cy.get('[data-testid="free-remove"]').should("exist");
+    cy.get('[data-testid="1-remove-delivery"]').should("exist");
+    cy.get('[data-testid="testservice-free-10to15yrs"]').should("exist");
+    cy.get('[data-testid="aidforchildrenwithtracheostomies"]').should("not.exist");
+    cy.get('[data-testid="testservice-paid-15to20yrs-afrikaans"]').should("not.exist");
+});
+
+When("user removes In Person", () => {
+    cy.get('[data-testid="1-remove-delivery"]').click();
+});
+
+Then("only one filter should exist", () => {
+    cy.get('[data-testid="free-remove"]').should("exist");
+    cy.get('[data-testid="1-remove-delivery"]').should("not.exist");
+    cy.get('[data-testid="testservice-free-10to15yrs"]').should("exist");
+    cy.get('[data-testid="aidforchildrenwithtracheostomies"]').should("exist");
+    cy.get('[data-testid="testservice-paid-15to20yrs-afrikaans"]').should("not.exist");
+});
+
+When("user clicks on clear all filters", () => {
+    cy.get('[data-testid="clearfilters"]').click();
+});
+
+Then("all filters should be cleared", () => {
+    cy.get('[data-testid="free-remove"]').should("not.exist");
+    cy.get('[data-testid="1-remove-delivery"]').should("not.exist");
+    cy.get('[data-testid="testservice-free-10to15yrs"]').should("exist");
+    cy.get('[data-testid="aidforchildrenwithtracheostomies"]').should("exist");
+    cy.get('[data-testid="testservice-paid-15to20yrs-afrikaans"]').should("exist");
+});
