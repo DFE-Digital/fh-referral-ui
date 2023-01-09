a26a4f0144cc11f59dc05968e5f82226424cb063
vipin <115160396+vreddydfe@users.noreply.github.com>
2023-01-09 16:14:08 +0000

    FHG-1739: Footer links added to the website (#91)

---
+++ src/FamilyHubs.ReferralUi.Ui/Pages/ProfessionalReferral/Accessibility.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FamilyHubs.ReferralUi.Ui.Pages.Shared
+{
+    public class Accessibility : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
