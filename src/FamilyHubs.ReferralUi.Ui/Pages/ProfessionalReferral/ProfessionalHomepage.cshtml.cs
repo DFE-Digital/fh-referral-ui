b94f5dd3888d3144b66d633ed0c98dcab1c54a65
Martin-Belton-gov <martin.belton@digital.education.gov.uk>
2023-02-15 12:18:12 +0000

    Added Additional Unit Tests and bug fixes

--- src/FamilyHubs.ReferralUi.Ui/Pages/ProfessionalReferral/ProfessionalHomepage.cshtml.cs
+++ src/FamilyHubs.ReferralUi.Ui/Pages/ProfessionalReferral/ProfessionalHomepage.cshtml.cs
@@ -1,9 +1,12 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Diagnostics.CodeAnalysis;
 
 namespace FamilyHubs.ReferralUi.Ui.Pages.ProfessionalReferral
 {
+    //No logic so no need for unit tests
+    [ExcludeFromCodeCoverage]
     [Authorize(Policy = "Referrer")]
     public class ProfessionalHomepageModel : PageModel
     {
--- src/FamilyHubs.ReferralUi.Ui/Pages/ProfessionalReferral/Termsandcondition.cshtml.cs
+++ src/FamilyHubs.ReferralUi.Ui/Pages/ProfessionalReferral/Termsandcondition.cshtml.cs
@@ -1,8 +1,10 @@
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Diagnostics.CodeAnalysis;
 
 namespace FamilyHubs.ReferralUi.Ui.Pages.ProfessionalReferral
 {
+    //No logic so no need for unit tests
+    [ExcludeFromCodeCoverage]
     public class TermsandconditionModel : PageModel
     {
        
