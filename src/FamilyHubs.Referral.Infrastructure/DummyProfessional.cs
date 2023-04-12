fc9cd9b4dc54f7689983864819e8c4b9e27c1c78
Martin-Belton-gov <martin.belton@digital.education.gov.uk>
2023-04-12 12:15:36 +0100

    Added Fake Professional

---
+++ src/FamilyHubs.Referral.Infrastructure/DummyProfessional.cs
@@ -0,0 +1,89 @@
+﻿using FamilyHubs.Referral.Core.ApiClients;
+using FamilyHubs.Referral.Core.Services;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Http;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace FamilyHubs.Referral.Infrastructure;
+
+//Tempory class for creating a Professional User
+
+public static class DummyProfessional
+{
+    public static string GetProfessionalUserToken()
+    {
+        var authClaims = new List<Claim>
+        {
+                    new Claim("BtlPro", "123"),
+                    new Claim(ClaimTypes.Email, "BtlPro@email.com"),
+                    new Claim(ClaimTypes.Name, "Bristol Professional"),
+                    new Claim(ClaimTypes.Role, "Professional"),
+                    new Claim("OpenReferralOrganisationId", "1"),
+                    new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+        };
+        JwtSecurityToken tokenItem = CreateToken(authClaims);
+        var token = new JwtSecurityTokenHandler().WriteToken(tokenItem);
+        return token;
+    }
+
+    public static async Task<long> LogUserIn(HttpContext httpContext, ITokenService tokenService, ClaimsPrincipal user, string token)
+    {
+        ArgumentNullException.ThrowIfNull(httpContext);
+        ArgumentNullException.ThrowIfNull(tokenService);
+        ArgumentNullException.ThrowIfNull(token);
+
+        long organisationId = 0;
+
+        var handler = new JwtSecurityTokenHandler();
+        var jwtSecurityToken = handler.ReadJwtToken(token);
+        var claims = jwtSecurityToken.Claims.ToList();
+
+        var claim = claims.FirstOrDefault(x => x.Type == "OpenReferralOrganisationId");
+        if (claim != null && long.TryParse(claim.Value, out long orgIdValue))
+        {
+            organisationId = orgIdValue;
+        }
+
+        var appIdentity = new ClaimsIdentity(claims);
+        user.AddIdentity(appIdentity);
+
+        //Initialize a new instance of the ClaimsIdentity with the claims and authentication scheme    
+        var identity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+        //Initialize a new instance of the ClaimsPrincipal with ClaimsIdentity    
+        var principal = new ClaimsPrincipal(identity);
+
+        tokenService.SetToken(token, jwtSecurityToken.ValidTo, "RefreshToken");
+
+        //SignInAsync is a Extension method for Sign in a principal for the specified scheme.    
+        await httpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principal, new AuthenticationProperties()
+        {
+            IsPersistent = false //Input.RememberMe,
+        });
+
+        return organisationId;
+    }
+
+    public static async Task LogUserOut(HttpContext httpContext, ITokenService tokenService)
+    {
+        await httpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+        tokenService.ClearTokens();
+    }
+    private static JwtSecurityToken CreateToken(List<Claim> authClaims)
+    {
+        var authSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("JWTAuthenticationHIGHsecuredPasswordVVVp1OH7Xzyr"));
+
+        var token = new JwtSecurityToken(
+            issuer: "https://localhost:7108",
+            audience: "MySuperSecureApiUser",
+            expires: DateTime.Now.AddMinutes(10),
+            claims: authClaims,
+            signingCredentials: new SigningCredentials(authSigningKey, SecurityAlgorithms.HmacSha256)
+            );
+
+        return token;
+    }
+}
---
+++ tests/FamilyHubs.ReferralUi.UnitTests/WhenUsingDummyProfessional.cs
@@ -0,0 +1,137 @@
+﻿using FamilyHubs.Referral.Core.ApiClients;
+using FamilyHubs.Referral.Core.Services;
+using FamilyHubs.Referral.Infrastructure;
+using FluentAssertions;
+using FluentAssertions.Common;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using Newtonsoft.Json.Linq;
+using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FamilyHubs.ReferralUi.UnitTests;
+
+public class WhenUsingDummyProfessional
+{
+    [Fact]
+    public void ThenGetProfessionalUserToken()
+    {
+        //Arrange and Act
+        string result = DummyProfessional.GetProfessionalUserToken();
+        var handler = new JwtSecurityTokenHandler();
+        var jwtSecurityToken = handler.ReadJwtToken(result);
+        var claims = jwtSecurityToken.Claims.ToList();
+        var idClaim = claims.FirstOrDefault(x => x.Type == "OpenReferralOrganisationId");
+        var emailClaim = claims.FirstOrDefault(x => x.Type == ClaimTypes.Email);
+
+        //Assert
+        ArgumentNullException.ThrowIfNull(idClaim);
+        ArgumentNullException.ThrowIfNull(emailClaim);
+        idClaim.Value.Should().Be("1");
+        emailClaim.Value.Should().Be("BtlPro@email.com");
+    }
+
+    [Fact]
+    public async Task ThenLogInProfessionalUser()
+    {
+        //Arrange
+        var user = new ClaimsPrincipal(new ClaimsIdentity(
+        new List<Claim>
+        {
+                    new Claim("BtlPro", "123"),
+                    new Claim(ClaimTypes.Email, "BtlPro@email.com"),
+                    new Claim(ClaimTypes.Name, "Bristol Professional"),
+                    new Claim(ClaimTypes.Role, "Professional"),
+                    new Claim("OpenReferralOrganisationId", "1"),
+                    new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+        }.ToArray(),
+       "mock"));
+
+        object item = default!;
+        var authServiceMock = new Mock<IAuthenticationService>();
+        authServiceMock
+            .Setup(_ => _.SignInAsync(It.IsAny<HttpContext>(), It.IsAny<string>(), It.IsAny<ClaimsPrincipal>(), It.IsAny<AuthenticationProperties>()))
+            .Returns(Task.FromResult(item));
+
+        var services = new ServiceCollection();
+        services.AddSingleton<IAuthenticationService>(authServiceMock.Object);
+
+        var httpContext = new DefaultHttpContext()
+        {
+            RequestServices = services.BuildServiceProvider(),
+            User = user
+        };
+
+        Mock<IDistributedCacheService> mockRedisCacheService = new Mock<IDistributedCacheService>();
+        int setCallback = 0;
+        mockRedisCacheService.Setup(x => x.StoreStringValue(It.IsAny<string>(), It.IsAny<string>())).Callback(() => setCallback++);
+        TokenService tokenService = new TokenService(mockRedisCacheService.Object);
+
+
+        //Act
+        long result = await DummyProfessional.LogUserIn(httpContext, tokenService, user, DummyProfessional.GetProfessionalUserToken());
+
+
+        //Assert
+        result.Should().Be(1);
+        setCallback.Should().Be(2);
+        
+    }
+
+    [Fact]
+    public async Task ThenLogoutProfessionalUser()
+    {
+        //Arrange
+        var user = new ClaimsPrincipal(new ClaimsIdentity(
+        new List<Claim>
+        {
+                    new Claim("BtlPro", "123"),
+                    new Claim(ClaimTypes.Email, "BtlPro@email.com"),
+                    new Claim(ClaimTypes.Name, "Bristol Professional"),
+                    new Claim(ClaimTypes.Role, "Professional"),
+                    new Claim("OpenReferralOrganisationId", "1"),
+                    new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+        }.ToArray(),
+       "mock"));
+
+        object item = default!;
+        var authServiceMock = new Mock<IAuthenticationService>();
+        authServiceMock
+            .Setup(_ => _.SignInAsync(It.IsAny<HttpContext>(), It.IsAny<string>(), It.IsAny<ClaimsPrincipal>(), It.IsAny<AuthenticationProperties>()))
+            .Returns(Task.FromResult(item));
+
+        var services = new ServiceCollection();
+        services.AddSingleton<IAuthenticationService>(authServiceMock.Object);
+
+        var httpContext = new DefaultHttpContext()
+        {
+            RequestServices = services.BuildServiceProvider(),
+            User = user
+        };
+
+        Mock<IDistributedCacheService> mockRedisCacheService = new Mock<IDistributedCacheService>();
+        int setCallback = 0;
+        mockRedisCacheService.Setup(x => x.StoreStringValue(It.IsAny<string>(), It.IsAny<string>())).Callback(() => setCallback++);
+        TokenService tokenService = new TokenService(mockRedisCacheService.Object);
+        long result = await DummyProfessional.LogUserIn(httpContext, tokenService, user, DummyProfessional.GetProfessionalUserToken());
+
+        //Act
+        await DummyProfessional.LogUserOut(httpContext, tokenService);
+
+
+
+        //Assert
+        result.Should().Be(1);
+        setCallback.Should().Be(4);
+
+    }
+}
