e2abd5e6c31971c7f198a65d1b6d4bb70fc10b33
Martin-Belton-gov <martin.belton@digital.education.gov.uk>
2023-04-13 11:37:22 +0100

    Adding Consent Page

---
+++ src/FamilyHubs.Referral.Web/Pages/ProfessionalReferral/Consent.cshtml.cs
@@ -0,0 +1,61 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FamilyHubs.Referral.Web.Pages.ProfessionalReferral;
+
+public class ConsentModel : PageModel
+{
+
+    [BindProperty]
+    public string Id { get; set; } = default!;
+    [BindProperty]
+    public string Name { get; set; } = default!;
+
+    [BindProperty]
+    public string IsConsentGiven { get; set; } = default!;
+
+
+    [BindProperty]
+    public bool ValidationValid { get; set; } = true;
+
+    public void OnGet(string id, string name)
+    {
+        Id = id;
+        Name = name;
+    }
+
+    public IActionResult OnPost(string id, string name)
+    {
+        if (!ModelState.IsValid || IsConsentGiven == null)
+        {
+            ValidationValid = false;
+            return Page();
+        }
+
+
+        if (string.Compare(IsConsentGiven, "yes", StringComparison.OrdinalIgnoreCase) == 0)
+        {
+            if (User != null && User.Identity != null && !User.Identity.IsAuthenticated)
+            {
+                return RedirectToPage("/ProfessionalReferral/SignIn", new
+                {
+                    id,
+                    name
+                });
+
+            }
+
+            return RedirectToPage("/ProfessionalReferral/FamilyContact", new
+            {
+                id,
+                name
+            });
+
+        }
+
+        return RedirectToPage("/ProfessionalReferral/ConsentShutter", new
+        {
+        });
+
+    }
+}
