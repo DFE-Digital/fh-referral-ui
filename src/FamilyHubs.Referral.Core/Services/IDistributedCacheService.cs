a2ec162b67c7341cebbb2b1387578ca7f5f77dc8
Martin-Belton-gov <martin.belton@digital.education.gov.uk>
2023-04-20 09:44:11 +0100

    Removed unused redis code

--- src/FamilyHubs.Referral.Core/Services/IDistributedCacheService.cs
+++ src/FamilyHubs.Referral.Core/Services/IDistributedCacheService.cs
@@ -1,22 +1,12 @@
 ﻿using FamilyHubs.Referral.Core.Models;
-using FamilyHubs.ServiceDirectory.Shared.Dto;
 
 namespace FamilyHubs.Referral.Core.Services;
 
 public interface IDistributedCacheService
 {
-    //Service
-    ServiceDto? RetrieveService();
-    void StoreService(ServiceDto serviceDto);
-
-    //Navigation - last page name
-    string RetrieveLastPageName();
-    void StoreCurrentPageName(string? currPage);
-    void ResetLastPageName();
-    void ResetOrganisationWithService();
     void StoreConnectWizzardViewModel(string key, ConnectWizzardViewModel value);
     ConnectWizzardViewModel RetrieveConnectWizzardViewModel(string key);
+
+    //This will be used on the last page after the data is saved to the api
     void ResetConnectWizzardViewModel(string key);
-    void StoreStringValue(string key, string value);
-    string RetrieveStringValue(string key);
 }
