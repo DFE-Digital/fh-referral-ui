ed0ae29c18f2534634cb144c3f5d9f728df4c2e7
Phil Davies <github@phildavies.co.uk>
2023-04-21 15:50:57 +0100

    unit tests wip

---
+++ tests/FamilyHubs.ReferralUi.UnitTests/Infrastructure/DistributedCache/DistributedCacheExtensionsTests.cs
@@ -0,0 +1,23 @@
+﻿using FluentAssertions;
+using FamilyHubs.Referral.Infrastructure.DistributedCache;
+using Microsoft.Extensions.Caching.Distributed;
+using Moq;
+
+namespace FamilyHubs.ReferralUi.UnitTests.Infrastructure.DistributedCache;
+
+public class DistributedCacheExtensionsTests
+{
+    Mock<IDistributedCache> DistributedCache = new();
+
+    [Fact]
+    public async Task GetAsync_WhenObjectIsNotInCache_ReturnsNull()
+    {
+        // Moq doesn't support mocking extension methods, so we have to mock internals of the extension method *ugh*
+        DistributedCache.Setup(x => x.GetAsync("key", default))
+            .ReturnsAsync((byte[]?)null);
+
+        var result = await DistributedCache.Object.GetAsync<object>("key");
+            
+        result.Should().Be(null);
+    }
+}
