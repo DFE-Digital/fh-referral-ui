7a8c2e3d7dc5cdd2234925790da03d933f512c10
Phil Davies <github@phildavies.co.uk>
2023-04-24 13:09:29 +0100

    unit tests wip

---
+++ tests/FamilyHubs.ReferralUi.UnitTests/Pages/ProfessionalReferral/BaseProfessionalReferralPage.cs
@@ -0,0 +1,25 @@
+﻿using FamilyHubs.Referral.Core.DistributedCache;
+using FamilyHubs.Referral.Core.Models;
+using Moq;
+
+namespace FamilyHubs.ReferralUi.UnitTests.Pages.ProfessionalReferral;
+
+public class BaseProfessionalReferralPage
+{
+    public Mock<IReferralDistributedCache> ReferralDistributedCache;
+    public readonly ProfessionalReferralModel ProfessionalReferralModel;
+
+    public BaseProfessionalReferralPage()
+    {
+        ProfessionalReferralModel = new ProfessionalReferralModel
+        {
+            ServiceId = "ServiceId",
+            ServiceName = "ServiceName",
+            FullName = "FullName",
+        };
+
+        ReferralDistributedCache = new Mock<IReferralDistributedCache>(MockBehavior.Strict);
+        ReferralDistributedCache.Setup(x => x.SetProfessionalReferralAsync(It.IsAny<ProfessionalReferralModel>())).Returns(Task.CompletedTask);
+        ReferralDistributedCache.Setup(x => x.GetProfessionalReferralAsync()).ReturnsAsync(ProfessionalReferralModel);
+    }
+}
--- tests/FamilyHubs.ReferralUi.UnitTests/Pages/ProfessionalReferral/WhenUsingSupportDetails.cs
+++ tests/FamilyHubs.ReferralUi.UnitTests/Pages/ProfessionalReferral/WhenUsingSupportDetails.cs
@@ -1,66 +1,63 @@
-﻿//using FamilyHubs.Referral.Web.Pages.ProfessionalReferral;
-//using FluentAssertions;
-//using Microsoft.AspNetCore.Http;
-//using Microsoft.AspNetCore.Mvc;
-//using Moq;
+﻿using FamilyHubs.Referral.Web.Pages.ProfessionalReferral;
+using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
 
-//namespace FamilyHubs.ReferralUi.UnitTests.Pages.ProfessionalReferral;
+namespace FamilyHubs.ReferralUi.UnitTests.Pages.ProfessionalReferral;
 
-//public class WhenUsingSupportDetails : BaseProfessionalReferralPage
-//{
-//    private readonly SupportDetailsModel _supportDetailsModel;
-//    public WhenUsingSupportDetails()
-//    {
-//        _supportDetailsModel = new SupportDetailsModel(_mockICacheService.Object);
-//    }
+public class WhenUsingSupportDetails : BaseProfessionalReferralPage
+{
+    private readonly SupportDetailsModel _supportDetailsModel;
+    public WhenUsingSupportDetails()
+    {
+        _supportDetailsModel = new SupportDetailsModel(ReferralDistributedCache.Object);
+    }
 
-//    [Fact]
-//    public void ThenOnGetSupportDetails()
-//    {
-//        //Arrange
-//        Mock<ISession> mockSession = new Mock<ISession>();
-//        var httpContext = new DefaultHttpContext() { Session = mockSession.Object };
-//        _supportDetailsModel.PageContext.HttpContext = httpContext;
+    [Fact]
+    public async Task ThenOnGetSupportDetails()
+    {
+        //Arrange
+        Mock<ISession> mockSession = new Mock<ISession>();
+        var httpContext = new DefaultHttpContext() { Session = mockSession.Object };
+        _supportDetailsModel.PageContext.HttpContext = httpContext;
 
+        //Act
+        await _supportDetailsModel.OnGetAsync("Id", "Some Name With Spaces");
 
-//        //Act
-//        _supportDetailsModel.OnGet("Id", "Some Name With Spaces");
+        //Assert
+        _supportDetailsModel.ServiceId.Should().Be("Id");
+        _supportDetailsModel.ServiceName.Should().Be("Some Name With Spaces");
 
-//        //Assert
-//        _supportDetailsModel.ServiceId.Should().Be("Id");
-//        _supportDetailsModel.ServiceName.Should().Be("Some Name With Spaces");
+    }
 
-//    }
+    [Fact]
+    public async Task ThenOnPostSupportDetails()
+    {
+        //Arrange
+        _supportDetailsModel.TextBoxValue = "Joe Blogs";
 
-//    [Fact]
-//    public void ThenOnPostSupportDetails()
-//    {
-//        //Arrange
-//        _supportDetailsModel.TextBoxValue = "Joe Blogs";
+        //Act
+        var result = await _supportDetailsModel.OnPostAsync("Id", "Some Name With Spaces") as RedirectToPageResult;
 
-//        //Act
-//        var result = _supportDetailsModel.OnPost() as RedirectToPageResult;
+        //Assert
+        ArgumentNullException.ThrowIfNull(result);
+        result.PageName.Should().Be("/ProfessionalReferral/WhySupport");
+    }
 
+    [Theory]
+    [InlineData(default!)]
+    [InlineData(" ")]
+    public async Task ThenOnPostSupportDetailsWithEmptyFullName(string value)
+    {
+        //Arrange
+        _supportDetailsModel.TextBoxValue = value;
+        _supportDetailsModel.ModelState.AddModelError("FullName", "Enter a full name");
 
-//        //Assert
-//        ArgumentNullException.ThrowIfNull(result);
-//        result.PageName.Should().Be("/ProfessionalReferral/WhySupport");
-//    }
+        //Act
+        await _supportDetailsModel.OnPostAsync("Id", "Some Name With Spaces");
 
-//    [Theory]
-//    [InlineData(default!)]
-//    [InlineData(" ")]
-//    public void ThenOnPostSupportDetailsWithEmptyFullName(string value)
-//    {
-//        //Arrange
-//        _supportDetailsModel.TextBoxValue = value;
-//        _supportDetailsModel.ModelState.AddModelError("FullName", "Enter a full name");
-
-//        //Act
-//        _supportDetailsModel.OnPost();
-
-
-//        //Assert
-//        _supportDetailsModel.PartialTextBoxViewModel.ValidationValid.Should().BeFalse();
-//    }
-//}
+        //Assert
+        _supportDetailsModel.PartialTextBoxViewModel.ValidationValid.Should().BeFalse();
+    }
+}
