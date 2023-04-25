e971a03c21e406bcda787b35ea646576aed92780
Phil Davies <github@phildavies.co.uk>
2023-04-25 15:12:38 +0100

    update unit tests

--- tests/FamilyHubs.ReferralUi.UnitTests/Pages/ProfessionalReferral/WhenUsingWhySupport.cs
+++ tests/FamilyHubs.ReferralUi.UnitTests/Pages/ProfessionalReferral/WhenUsingWhySupport.cs
@@ -1,77 +1,70 @@
-﻿//using FamilyHubs.Referral.Web.Pages.ConnectionRequest;
-//using FluentAssertions;
-//using Microsoft.AspNetCore.Mvc;
-//using Moq;
+﻿using FamilyHubs.Referral.Core.DistributedCache;
+using FamilyHubs.Referral.Core.Models;
+using FamilyHubs.Referral.Web.Pages.ProfessionalReferral;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
 
-//namespace FamilyHubs.ReferralUi.UnitTests.Pages.ConnectionRequest;
+namespace FamilyHubs.ReferralUi.UnitTests.Pages.ConnectionRequest;
 
-//public class WhenUsingWhySupport
-//{
-//    private readonly WhySupportModel _whySupportModel;
-//    private readonly Mock<IDistributedCacheService> _mockCacheService;
-//    private readonly ConnectWizzardViewModel _connectWizzardViewModel;
-//    public WhenUsingWhySupport()
-//    {
-//        _connectWizzardViewModel = new ConnectWizzardViewModel
-//        {
-//            ServiceId = "Service Id",
-//            ServiceName = "Service Name",
-//            FamilyContactFullName = "Full Name",
-//            Reason = "Reason for Support"
-//        };
-//        _mockCacheService = new Mock<IDistributedCacheService>();
-//        _whySupportModel = new WhySupportModel(_mockCacheService.Object);
-//    }
+public class WhenUsingWhySupport
+{
+    private readonly WhySupportModel _whySupportModel;
+    private readonly Mock<IConnectionRequestDistributedCache> _mockConnectionRequestDistributedCache;
+    private readonly ConnectionRequestModel _connectionRequestModel;
+    public WhenUsingWhySupport()
+    {
+        _connectionRequestModel = new ConnectionRequestModel
+        {
+            ServiceId = "Service Id",
+            ServiceName = "Service Name",
+            FamilyContactFullName = "Full Name",
+            Reason = "Reason for Support"
+        };
+        _mockConnectionRequestDistributedCache = new Mock<IConnectionRequestDistributedCache>();
+        _whySupportModel = new WhySupportModel(_mockConnectionRequestDistributedCache.Object);
+    }
 
-//    [Fact]
-//    public void ThenOnGetWhySupport()
-//    {
-//        //Arrange
-//        _mockCacheService.Setup(x => x.RetrieveConnectWizzardViewModel(It.IsAny<string>())).Returns(_connectWizzardViewModel);
+    [Fact]
+    public async Task ThenOnGetWhySupport()
+    {
+        _mockConnectionRequestDistributedCache.Setup(x => x.GetAsync()).ReturnsAsync(_connectionRequestModel);
 
-//        //Act
-//        _whySupportModel.OnGet();
+        //Act
+        await _whySupportModel.OnGetAsync("1");
 
-//        //Assert
-//        _whySupportModel.ServiceId.Should().Be(_connectWizzardViewModel.ServiceId);
-//        _whySupportModel.ServiceName.Should().Be(_connectWizzardViewModel.ServiceName);
-//        _whySupportModel.TextAreaValue.Should().Be(_connectWizzardViewModel.Reason);
+        _whySupportModel.ServiceId.Should().Be(_connectionRequestModel.ServiceId);
+        _whySupportModel.ServiceName.Should().Be(_connectionRequestModel.ServiceName);
+        _whySupportModel.TextAreaValue.Should().Be(_connectionRequestModel.Reason);
+    }
 
-//    }
+    [Fact]
+    public async Task ThenOnPostWhySupport()
+    {
+        _mockConnectionRequestDistributedCache.Setup(x => x.GetAsync()).ReturnsAsync(_connectionRequestModel);
+        _whySupportModel.TextAreaValue = "New Reason For Support";
 
-//    [Fact]
-//    public void ThenOnPostWhySupport()
-//    {
-//        //Arrange
-//        _mockCacheService.Setup(x => x.RetrieveConnectWizzardViewModel(It.IsAny<string>())).Returns(_connectWizzardViewModel);
-//        int callBack = 0;
-//        _mockCacheService.Setup(x => x.StoreConnectWizzardViewModel(It.IsAny<string>(),It.IsAny<ConnectWizzardViewModel>())).Callback(() => callBack++);
-//        _whySupportModel.TextAreaValue = "Reason For Support";
+        //Act
+        var result = await _whySupportModel.OnPostAsync() as RedirectToPageResult;
 
-//        //Act
-//        var result = _whySupportModel.OnPost() as RedirectToPageResult;
+        //todo: check new content
+        _mockConnectionRequestDistributedCache
+            .Verify(x => x.SetAsync(It.IsAny<ConnectionRequestModel>()), Times.Once);
+            
+        ArgumentNullException.ThrowIfNull(result);
+        result.PageName.Should().Be("/ProfessionalReferral/ContactDetails");
+    }
 
+    [Theory]
+    [InlineData(default)]
+    [InlineData("")]
+    public async Task ThenOnPostSupportDetailsWithEmptyFullName(string? value)
+    {
+        _whySupportModel.TextAreaValue = value;
 
-//        //Assert
-//        callBack.Should().Be(1);
-//        ArgumentNullException.ThrowIfNull(result);
-//        result.PageName.Should().Be("/ProfessionalReferral/ContactDetails");
-//    }
+        //Act
+        await _whySupportModel.OnPostAsync();
 
-//    [Theory]
-//    [InlineData(default!)]
-//    [InlineData(" ")]
-//    public void ThenOnPostSupportDetailsWithEmptyFullName(string value)
-//    {
-//        //Arrange
-//        _whySupportModel.TextAreaValue = value;
-//        _whySupportModel.ModelState.AddModelError("Text Area", "Enter a Reason For Support");
-
-//        //Act
-//        _whySupportModel.OnPost();
-
-
-//        //Assert
-//        _whySupportModel.ValidationValid.Should().BeFalse();
-//    }
-//}
+        _whySupportModel.TextAreaValidation.Should().Be(TextAreaValidation.Empty);
+    }
+}
