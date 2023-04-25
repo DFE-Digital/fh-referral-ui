8ca1e1f026fe0e428d0b747c0dca13cf7ed86e8e
Phil Davies <github@phildavies.co.uk>
2023-04-25 15:25:55 +0100

    fix nullability handling

--- src/FamilyHubs.Referral.Core/Models/ConnectionRequestModel.cs
+++ src/FamilyHubs.Referral.Core/Models/ConnectionRequestModel.cs
@@ -3,8 +3,8 @@ namespace FamilyHubs.Referral.Core.Models;
 
 public class ConnectionRequestModel
 {
-    public string ServiceId { get; set; } = string.Empty;
-    public string ServiceName { get; set; } = string.Empty;
-    public string FamilyContactFullName { get; set; } = string.Empty;
-    public string Reason { get; set; } = string.Empty;
+    public string? ServiceId { get; set; }
+    public string? ServiceName { get; set; }
+    public string? FamilyContactFullName { get; set; }
+    public string? Reason { get; set; }
 }
