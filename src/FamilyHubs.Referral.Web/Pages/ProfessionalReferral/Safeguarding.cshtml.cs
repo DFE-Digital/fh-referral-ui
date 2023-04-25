b580d2fa3978c4c6d13d886a9cb25d904ec77a24
Phil Davies <github@phildavies.co.uk>
2023-04-25 11:47:02 +0100

    tidy up

--- src/FamilyHubs.Referral.Web/Pages/ProfessionalReferral/Safeguarding.cshtml.cs
+++ src/FamilyHubs.Referral.Web/Pages/ProfessionalReferral/Safeguarding.cshtml.cs
@@ -1,14 +1,12 @@
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace FamilyHubs.Referral.Web.Pages.ProfessionalReferral;
 
 public class SafeguardingModel : PageModel
 {
-    [BindProperty]
-    public string ServiceId { get; set; } = default!;
-    [BindProperty]
-    public string ServiceName { get; set; } = default!;
+    public string? ServiceId { get; set; }
+    public string? ServiceName { get; set; }
+
     public void OnGet(string serviceId, string serviceName)
     {
         ServiceId = serviceId;
