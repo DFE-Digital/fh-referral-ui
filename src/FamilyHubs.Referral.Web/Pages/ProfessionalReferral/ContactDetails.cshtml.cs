3e0f1ae7cef8f505de599079c991be393f754680
Phil Davies <github@phildavies.co.uk>
2023-04-26 13:23:47 +0100

    fix build, following merge

--- src/FamilyHubs.Referral.Web/Pages/ProfessionalReferral/ContactDetails.cshtml.cs
+++ src/FamilyHubs.Referral.Web/Pages/ProfessionalReferral/ContactDetails.cshtml.cs
@@ -1,5 +1,4 @@
-using FamilyHubs.Referral.Core.Models;
-using FamilyHubs.Referral.Core.Services;
+using FamilyHubs.Referral.Core.DistributedCache;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -7,7 +6,7 @@ namespace FamilyHubs.Referral.Web.Pages.ProfessionalReferral;
 
 public class ContactDetailsModel : PageModel
 {
-    private readonly IDistributedCacheService _distributedCacheService;
+    private readonly IConnectionRequestDistributedCache _connectionRequestDistributedCache;
     public bool ValidationValid { get; private set; } = true;
     public string? FullName { get; set; }
 
@@ -23,21 +22,26 @@ public class ContactDetailsModel : PageModel
     [BindProperty]
     public string? Letter { get; set; }
 
-    public ContactDetailsModel(IDistributedCacheService distributedCacheService)
+    public ContactDetailsModel(IConnectionRequestDistributedCache connectionRequestDistributedCache)
     {
-        _distributedCacheService = distributedCacheService;
+        _connectionRequestDistributedCache = connectionRequestDistributedCache;
     }
-    public void OnGet()
+
+    public async Task OnGetAsync()
     {
-        ConnectWizzardViewModel model = _distributedCacheService.RetrieveConnectWizzardViewModel(TempStorageConfiguration.KeyConnectWizzardViewModel);
-        FullName = !string.IsNullOrEmpty(model.FullName) ? model.FullName : "Family";
+        var model = await _connectionRequestDistributedCache.GetAsync();
+        //todo: handle missing model. have base class for all pages that handles this?
+
+        //todo: why default to "Family"?
+        //FullName = !string.IsNullOrEmpty(model.FullName) ? model.FullName : "Family";
+        FullName = model!.FamilyContactFullName;
         Email = (model.EmailSelected) ? "Email" : null;
         Telephone = (model.TelephoneSelected) ? "Telephone" : null;
         Textphone = (model.TextPhoneSelected) ? "Textphone" : null;
         Letter = (model.LetterSelected) ? "Letter" : null;
     }
 
-    public IActionResult OnPost()
+    public async Task<IActionResult> OnPostAsync()
     {
         if (!ModelState.IsValid || (string.IsNullOrEmpty(Email) && string.IsNullOrEmpty(Telephone) && string.IsNullOrEmpty(Textphone) && string.IsNullOrEmpty(Letter)))
         {
@@ -46,12 +50,13 @@ public class ContactDetailsModel : PageModel
             return Page();
         }
         
-        ConnectWizzardViewModel model = _distributedCacheService.RetrieveConnectWizzardViewModel(TempStorageConfiguration.KeyConnectWizzardViewModel);
-        model.EmailSelected = !string.IsNullOrEmpty(Email);
+        var model = await _connectionRequestDistributedCache.GetAsync();
+        //todo: handle missing model
+        model!.EmailSelected = !string.IsNullOrEmpty(Email);
         model.TelephoneSelected = !string.IsNullOrEmpty(Telephone);
         model.TextPhoneSelected = !string.IsNullOrEmpty(Textphone);
         model.LetterSelected = !string.IsNullOrEmpty(Letter);
-        _distributedCacheService.StoreConnectWizzardViewModel(TempStorageConfiguration.KeyConnectWizzardViewModel,model);
+        await _connectionRequestDistributedCache.SetAsync(model);
 
         string destination = string.Empty;
         if (model.EmailSelected)
