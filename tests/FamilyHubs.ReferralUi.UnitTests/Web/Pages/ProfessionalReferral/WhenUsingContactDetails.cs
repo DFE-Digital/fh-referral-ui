2ed3b707b2a1b79e5aa4bd8fbc79bed89a93942f
Phil Davies <github@phildavies.co.uk>
2023-04-27 16:04:24 +0100

    unit tests wip

--- tests/FamilyHubs.ReferralUi.UnitTests/Web/Pages/ProfessionalReferral/WhenUsingContactDetails.cs
+++ tests/FamilyHubs.ReferralUi.UnitTests/Web/Pages/ProfessionalReferral/WhenUsingContactDetails.cs
@@ -1,73 +1,97 @@
-﻿//using FamilyHubs.Referral.Web.Pages.ProfessionalReferral;
-//using FluentAssertions;
-//using Microsoft.AspNetCore.Mvc;
+﻿using FamilyHubs.Referral.Core.DistributedCache;
+using FamilyHubs.Referral.Core.Models;
+using FamilyHubs.Referral.Web.Pages.ProfessionalReferral;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
 
-//namespace FamilyHubs.ReferralUi.UnitTests.Pages.ProfessionalReferral;
+namespace FamilyHubs.ReferralUi.UnitTests.Web.Pages.ProfessionalReferral;
 
-//public class WhenUsingContactDetails : BaseProfessionalReferralPage
-//{
-//    private readonly ContactDetailsModel _contactDetailsModel;
+public class WhenUsingContactDetails : BaseProfessionalReferralPage
+{
+    private readonly ContactDetailsModel _contactDetailsModel;
+    private readonly Mock<IConnectionRequestDistributedCache> _mockConnectionRequestDistributedCache;
+    private readonly ConnectionRequestModel _connectionRequestModel;
 
-//    public WhenUsingContactDetails()
-//    {
-//        _contactDetailsModel = new ContactDetailsModel(_mockICacheService.Object);
-//    }
+    public WhenUsingContactDetails()
+    {
+        _connectionRequestModel = new ConnectionRequestModel
+        {
+            ServiceId = "Service Id",
+            ServiceName = "Service Name",
+            FamilyContactFullName = "Full Name",
+            Reason = "Reason for Support",
+        };
+        _mockConnectionRequestDistributedCache = new Mock<IConnectionRequestDistributedCache>();
+        _mockConnectionRequestDistributedCache.Setup(x => x.GetAsync()).ReturnsAsync(_connectionRequestModel);
 
-//    [Fact]
-//    public void ThenOnGetSupportDetails()
-//    {
-//        //Arrange and Act
-//        _contactDetailsModel.OnGet();
+        _contactDetailsModel = new ContactDetailsModel(_mockConnectionRequestDistributedCache.Object);
+    }
 
-//        //Assert
-//        _contactDetailsModel.Email.Should().Be("Email");
-//        _contactDetailsModel.Telephone.Should().Be("Telephone");
-//        _contactDetailsModel.Textphone.Should().Be("Textphone");
-//        _contactDetailsModel.Letter.Should().Be("Letter");
+    [Theory]
+    [InlineData(false, false, false, false)]
+    [InlineData(true, false, false, false)]
+    [InlineData(false, true, false, false)]
+    [InlineData(false, false, true, false)]
+    [InlineData(false, false, false, true)]
+    [InlineData(true, true, true, true)]
+    public async Task ThenCheckboxesShouldMatchRetrievedModel(bool email, bool telephone, bool textphone, bool letter)
+    {
+        _connectionRequestModel.EmailSelected = email;
+        _connectionRequestModel.TelephoneSelected = telephone;
+        _connectionRequestModel.TextPhoneSelected = textphone;
+        _connectionRequestModel.LetterSelected = letter;
 
-//    }
+        //Act
+        await _contactDetailsModel.OnGetAsync("1");
 
-//    [Theory]
-//    [InlineData("Email", "/ProfessionalReferral/Email")]
-//    [InlineData("Telephone", "/ProfessionalReferral/Telephone")]
-//    [InlineData("Textphone", "/ProfessionalReferral/Textphone")]
-//    [InlineData("Letter", "/ProfessionalReferral/Letter")]
-//    public void ThenOnPostSupportDetails(string value, string urlDesination)
-//    {
-//        //Arrange
-//        switch(value)
-//        {
-//            case "Telephone":
-//                _contactDetailsModel.Telephone = "Telephone";
-//            break;
-//            case "Textphone":
-//                _contactDetailsModel.Textphone = "Textphone";
-//                break;
-//            case "Letter":
-//                _contactDetailsModel.Letter = "Letter";
-//                break;
-//            default:
-//                _contactDetailsModel.Email = "Email";
-//                break;
-//        }
-        
+        _contactDetailsModel.Email.Should().Be(email);
+        _contactDetailsModel.Telephone.Should().Be(telephone);
+        _contactDetailsModel.Textphone.Should().Be(textphone);
+        _contactDetailsModel.Letter.Should().Be(letter);
+    }
 
-//        //Act
-//        var result = _contactDetailsModel.OnPost() as RedirectToPageResult;
+    //[Theory]
+    //[InlineData("Email", "/ProfessionalReferral/Email")]
+    //[InlineData("Telephone", "/ProfessionalReferral/Telephone")]
+    //[InlineData("Textphone", "/ProfessionalReferral/Textphone")]
+    //[InlineData("Letter", "/ProfessionalReferral/Letter")]
+    //public void ThenOnPostSupportDetails(string value, string urlDesination)
+    //{
+    //    //Arrange
+    //    switch (value)
+    //    {
+    //        case "Telephone":
+    //            _contactDetailsModel.Telephone = "Telephone";
+    //            break;
+    //        case "Textphone":
+    //            _contactDetailsModel.Textphone = "Textphone";
+    //            break;
+    //        case "Letter":
+    //            _contactDetailsModel.Letter = "Letter";
+    //            break;
+    //        default:
+    //            _contactDetailsModel.Email = "Email";
+    //            break;
+    //    }
 
 
-//        //Assert
-//        ArgumentNullException.ThrowIfNull(result);
-//        result.PageName.Should().Be(urlDesination);
-//    }
+    //    //Act
+    //    var result = _contactDetailsModel.OnPost() as RedirectToPageResult;
 
-//    [Fact]
-//    public void ThenOnPostWithValidationError() 
-//    {
-//        //Arrange and Act
-//        var result = _contactDetailsModel.OnPost() as RedirectToPageResult;
 
-//        //Assert
-//        _contactDetailsModel.ValidationValid.Should().BeFalse();
-//    }
-//}
+    //    //Assert
+    //    ArgumentNullException.ThrowIfNull(result);
+    //    result.PageName.Should().Be(urlDesination);
+    //}
+
+    //[Fact]
+    //public void ThenOnPostWithValidationError()
+    //{
+    //    //Arrange and Act
+    //    var result = _contactDetailsModel.OnPost() as RedirectToPageResult;
+
+    //    //Assert
+    //    _contactDetailsModel.ValidationValid.Should().BeFalse();
+    //}
+}
