a5c6509e5d30358bd32d4db44ece815af0ae806a
Phil Davies <github@phildavies.co.uk>
2023-05-05 14:20:11 +0100

    letter wip

--- src/FamilyHubs.Referral.Web/Pages/ProfessionalReferral/Letter.cshtml.cs
+++ src/FamilyHubs.Referral.Web/Pages/ProfessionalReferral/Letter.cshtml.cs
@@ -1,12 +1,30 @@
+using System.ComponentModel.DataAnnotations;
 using FamilyHubs.Referral.Core.DistributedCache;
 using FamilyHubs.Referral.Core.Models;
 using FamilyHubs.Referral.Web.Pages.Shared;
+using Microsoft.AspNetCore.Mvc;
 
 namespace FamilyHubs.Referral.Web.Pages.ProfessionalReferral;
 
 public class LetterModel : ProfessionalReferralSessionModel
 {
+    //todo: consistency with nullable
+    [BindProperty]
+    [Required]
+    public string AddressLine1 { get; set; } = "";
+    [BindProperty]
+    public string AddressLine2 { get; set; } = "";
+    [BindProperty]
+    [Required]
+    public string TownOrCity { get; set; } = "";
+    [BindProperty]
+    public string County { get; set; } = "";
+    [BindProperty]
+    [Required]
+    public string Postcode { get; set; } = "";
+
     public string HeadingText { get; set; } = "";
+    public string? BackUrl { get; set; }
 
     public LetterModel(IConnectionRequestDistributedCache connectionRequestCache)
         : base(connectionRequestCache)
@@ -33,5 +51,6 @@ public class LetterModel : ProfessionalReferralSessionModel
     private void SetPageProperties(ConnectionRequestModel model)
     {
         HeadingText = $"What is the address for {model.FamilyContactFullName}?";
+        BackUrl = PreviousPage(ContactMethod.Letter, model.ContactMethodsSelected);
     }
 }
