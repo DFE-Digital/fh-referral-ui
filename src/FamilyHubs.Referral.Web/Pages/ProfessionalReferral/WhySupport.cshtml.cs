cfb22c78535f1b0f952c83bda563a9b2f9e9f108
Phil Davies <github@phildavies.co.uk>
2023-05-11 13:55:03 +0100

    update error message when user hasn't entered details about the family

--- src/FamilyHubs.Referral.Web/Pages/ProfessionalReferral/WhySupport.cshtml.cs
+++ src/FamilyHubs.Referral.Web/Pages/ProfessionalReferral/WhySupport.cshtml.cs
@@ -29,7 +29,7 @@ public class WhySupportModel : ProfessionalReferralSessionModel, ITellTheService
     {
         if (string.IsNullOrEmpty(TextAreaValue))
         {
-            TextAreaValidationErrorMessage = "Enter reason for the connection request";
+            TextAreaValidationErrorMessage = "Enter details about the family";
             return null;
         }
 
