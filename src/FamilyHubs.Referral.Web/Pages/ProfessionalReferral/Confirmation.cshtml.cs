38ca1b27cf08acd77cf34e8af42877f8982f8c69
Phil Davies <github@phildavies.co.uk>
2023-05-30 11:11:12 +0100

    confirmation page was not authorisation protected and didn't have the menu in the header

--- src/FamilyHubs.Referral.Web/Pages/ProfessionalReferral/Confirmation.cshtml.cs
+++ src/FamilyHubs.Referral.Web/Pages/ProfessionalReferral/Confirmation.cshtml.cs
@@ -1,13 +1,21 @@
+using FamilyHubs.SharedKernel.Razor.FamilyHubsUi.Delegators;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace FamilyHubs.Referral.Web.Pages.ProfessionalReferral;
 
-public class ConfirmationModel : PageModel
+[Authorize]
+public class ConfirmationModel : PageModel, IFamilyHubsHeader
 {
     public int RequestNumber { get; set; }
 
     public void OnGet(int requestNumber)
     {
         RequestNumber = requestNumber;
+    }
+
+    LinkStatus IFamilyHubsHeader.GetStatus(SharedKernel.Razor.FamilyHubsUi.Options.LinkOptions link)
+    {
+        return link.Text == "Search for service" ? LinkStatus.Active : LinkStatus.Visible;
     }
 }
