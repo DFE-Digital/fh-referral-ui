d05aa648676f2211330b24995494e4b51d857cc7
Phil Davies <github@phildavies.co.uk>
2023-06-27 11:59:44 +0100

    wip

--- src/FamilyHubs.Referral.Web/Errors/IErrorSummary.cs
+++ src/FamilyHubs.Referral.Web/Errors/IErrorSummary.cs
@@ -2,18 +2,22 @@
 
 public record Error(int Id, string HtmlElementId, string ErrorMessage);
 
-//todo: need a composable helper class to implement this interface
+//todo: better naming of all these. GetError is the odd one out as it's not referring to the current state
+//todo: single interface? copy methods? better naming?
+
 public interface IErrorSummary
 {
-    //todo: better naming of all these. GetError is the odd one out as it's not referring to the current state
     bool HasErrors { get; }
-    bool HasError(params int[] errorIds);
-
     IEnumerable<int> ErrorIds { get; }
-
-    int? GetErrorIdIfTriggered(params int[] mutuallyExclusiveErrorIds);
-    Error? GetErrorIfTriggered(params int[] mutuallyExclusiveErrorIds);
 
     //todo: leave this out of interface? (GetErrorIfTriggered could be used instead (renamed), but consumers would have to deal with null) or just rename?
     Error GetError(int errorId);
+}
+
+public interface IErrorState : IErrorSummary
+{
+    bool HasError(params int[] errorIds);
+
+    int? GetErrorIdIfTriggered(params int[] mutuallyExclusiveErrorIds);
+    Error? GetErrorIfTriggered(params int[] mutuallyExclusiveErrorIds);
 }
--- src/FamilyHubs.Referral.Web/Pages/ProfessionalReferral/ContactByPhone.cshtml.cs
+++ src/FamilyHubs.Referral.Web/Pages/ProfessionalReferral/ContactByPhone.cshtml.cs
@@ -1,3 +1,4 @@
+using System.Collections.Immutable;
 using System.ComponentModel.DataAnnotations;
 using System.Diagnostics.CodeAnalysis;
 using FamilyHubs.Referral.Core.DistributedCache;
@@ -9,15 +10,73 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace FamilyHubs.Referral.Web.Pages.ProfessionalReferral;
 
-public class ContactByPhoneModel : ProfessionalReferralCacheModel, IErrorSummary
+//todo: combine error handling from base class into this too?
+public class ErrorState : IErrorSummary
 {
+    private readonly ImmutableDictionary<int, Error> _possibleErrors;
+
+    public ErrorState(ImmutableDictionary<int, Error> possibleErrors, IEnumerable<int> triggeredErrors)
+    {
+        _possibleErrors = possibleErrors;
+        ErrorIds = triggeredErrors;
+    }
+
+    public bool HasErrors => ErrorIds.Any();
+
+    public IEnumerable<int> ErrorIds { get; }
+
+    public Error GetError(int errorId)
+    {
+        return _possibleErrors[errorId];
+    }
+
+    public bool HasError(params int[] errorIds)
+    {
+        return GetErrorIdIfTriggered(errorIds) != null;
+    }
+
+    [SuppressMessage("Minor Code Smell", "S3267:Loops should be simplified with \"LINQ\" expressions", Justification = "LINQ expression version is less simple")]
+    public int? GetErrorIdIfTriggered(params int[] mutuallyExclusiveErrorIds)
+    {
+        if (!mutuallyExclusiveErrorIds.Any())
+        {
+            return ErrorIds.Any() ? ErrorIds.First() : null;
+        }
+
+        foreach (int errorId in mutuallyExclusiveErrorIds)
+        {
+            if (ErrorIds.Contains(errorId))
+            {
+                return errorId;
+            }
+        }
+
+        return null;
+    }
+
+    public Error? GetErrorIfTriggered(params int[] mutuallyExclusiveErrorIds)
+    {
+        int? currentErrorId = GetErrorIdIfTriggered(mutuallyExclusiveErrorIds);
+        return currentErrorId != null ? GetError(currentErrorId.Value) : null;
+    }
+}
+
+public class ContactByPhoneModel : ProfessionalReferralCacheModel//, IErrorSummary
+{
+    // one of these for all errors?
+    private static readonly ImmutableDictionary<int, Error> PossibleErrors = ImmutableDictionary
+        .Create<int, Error>()
+        .Add((int)ProfessionalReferralError.ContactByPhone_NoContactSelected, new Error((int)ProfessionalReferralError.ContactByPhone_NoContactSelected, "email", "Select how the service can contact you"))
+        .Add((int)ProfessionalReferralError.ContactByPhone_NoTelephoneNumber, new Error((int)ProfessionalReferralError.ContactByPhone_NoTelephoneNumber, "contact-by-phone", "Enter a UK telephone number"))
+        .Add((int)ProfessionalReferralError.ContactByPhone_InvalidTelephoneNumber, new Error((int)ProfessionalReferralError.ContactByPhone_InvalidTelephoneNumber, "contact-by-phone", "Enter a telephone number, like 01632 960 001, 07700 900 982 or +44 808 157 0192"));
+
+    public ErrorState? ErrorState { get; private set; }
+
     [BindProperty]
     public string? TelephoneNumber { get; set; }
 
     [BindProperty]
     public ReferrerContactType? Contact { get; set; }
-
-    public string? ErrorMessage { get; set; }
 
     public ContactByPhoneModel(IConnectionRequestDistributedCache connectionRequestDistributedCache)
         : base(ConnectJourneyPage.ContactByPhone, connectionRequestDistributedCache)
@@ -26,7 +85,12 @@ public class ContactByPhoneModel : ProfessionalReferralCacheModel, IErrorSummary
 
     protected override void OnGetWithModel(ConnectionRequestModel model)
     {
-        if (!HasErrors && model.ReferrerContact != null)
+        if (HasErrors)
+        {
+            ErrorState = new ErrorState(PossibleErrors,
+                ConnectionRequestModel?.ErrorState?.Errors.Select(e => (int) e) ?? Enumerable.Empty<int>());
+        }
+        else if (model.ReferrerContact != null)
         {
             Contact = model.ReferrerContact;
             TelephoneNumber = model.TelephoneNumber;
@@ -58,78 +122,78 @@ public class ContactByPhoneModel : ProfessionalReferralCacheModel, IErrorSummary
         }
 
         model.ReferrerContact = Contact;
-        model.ReferrerTelephone = TelephoneNumber;
+        model.ReferrerTelephone = Contact == ReferrerContactType.TelephoneAndEmail ? TelephoneNumber : null;
 
         return NextPage();
     }
 
-    //todo: work these into base
-    public IEnumerable<int> ErrorIds
-    {
-        get
-        {
-            return ConnectionRequestModel?.ErrorState?.Errors.Select(e => (int) e) ?? Enumerable.Empty<int>();
-        }
-    }
+//    //todo: work these into base
+//    public IEnumerable<int> ErrorIds
+//    {
+//        get
+//        {
+//            return ConnectionRequestModel?.ErrorState?.Errors.Select(e => (int) e) ?? Enumerable.Empty<int>();
+//        }
+//    }
 
-    public Error GetError(int errorId)
-    {
-        //todo: have a static immutable dictionary
-        return (ProfessionalReferralError) errorId switch
-        {
-            ProfessionalReferralError.ContactByPhone_NoContactSelected
-                => new Error((int)ProfessionalReferralError.ContactByPhone_NoContactSelected, "email", "Select how the service can contact you"),
-            ProfessionalReferralError.ContactByPhone_NoTelephoneNumber
-                => new Error((int)ProfessionalReferralError.ContactByPhone_NoTelephoneNumber, "contact-by-phone", "Enter a UK telephone number"),
-            ProfessionalReferralError.ContactByPhone_InvalidTelephoneNumber
-                => new Error((int)ProfessionalReferralError.ContactByPhone_InvalidTelephoneNumber, "contact-by-phone", "Enter a telephone number, like 01632 960 001, 07700 900 982 or +44 808 157 0192"),
-            _ => throw new NotImplementedException()
-        };
-    }
+//    public Error GetError(int errorId)
+//    {
+//        //todo: have a static immutable dictionary
+//        return (ProfessionalReferralError) errorId switch
+//        {
+//            ProfessionalReferralError.ContactByPhone_NoContactSelected
+//                => new Error((int)ProfessionalReferralError.ContactByPhone_NoContactSelected, "email", "Select how the service can contact you"),
+//            ProfessionalReferralError.ContactByPhone_NoTelephoneNumber
+//                => new Error((int)ProfessionalReferralError.ContactByPhone_NoTelephoneNumber, "contact-by-phone", "Enter a UK telephone number"),
+//            ProfessionalReferralError.ContactByPhone_InvalidTelephoneNumber
+//                => new Error((int)ProfessionalReferralError.ContactByPhone_InvalidTelephoneNumber, "contact-by-phone", "Enter a telephone number, like 01632 960 001, 07700 900 982 or +44 808 157 0192"),
+//            _ => throw new NotImplementedException()
+//        };
+//    }
 
-    //public bool HasError(int errorId)
-    //{
-    //    return ErrorIds.Contains(errorId);
-    //}
+//    //public bool HasError(int errorId)
+//    //{
+//    //    return ErrorIds.Contains(errorId);
+//    //}
 
-    public bool HasError(params int[] errorIds)
-    {
-        return GetErrorIdIfTriggered(errorIds) != null;
-//#pragma warning disable S3267 // Loops should be simplified with "LINQ" expressions
-//        foreach (int errorId in errorIds)
+//    public bool HasError(params int[] errorIds)
+//    {
+//        return GetErrorIdIfTriggered(errorIds) != null;
+////#pragma warning disable S3267 // Loops should be simplified with "LINQ" expressions
+////        foreach (int errorId in errorIds)
+////        {
+////            if (ErrorIds.Contains(errorId))
+////            {
+////                return true;
+////            }
+////        }
+////#pragma warning restore S3267 // Loops should be simplified with "LINQ" expressions
+
+////        return false;
+//    }
+
+//    [SuppressMessage("Minor Code Smell", "S3267:Loops should be simplified with \"LINQ\" expressions", Justification = "LINQ expression version is less simple")]
+//    public int? GetErrorIdIfTriggered(params int[] mutuallyExclusiveErrorIds)
+//    {
+//        if (!mutuallyExclusiveErrorIds.Any())
+//        {
+//            return ErrorIds.Any() ? ErrorIds.First() : null;
+//        }
+
+//        foreach (int errorId in mutuallyExclusiveErrorIds)
 //        {
 //            if (ErrorIds.Contains(errorId))
 //            {
-//                return true;
+//                return errorId;
 //            }
 //        }
-//#pragma warning restore S3267 // Loops should be simplified with "LINQ" expressions
 
-//        return false;
-    }
+//        return null;
+//    }
 
-    [SuppressMessage("Minor Code Smell", "S3267:Loops should be simplified with \"LINQ\" expressions", Justification = "LINQ expression version is less simple")]
-    public int? GetErrorIdIfTriggered(params int[] mutuallyExclusiveErrorIds)
-    {
-        if (!mutuallyExclusiveErrorIds.Any())
-        {
-            return ErrorIds.Any() ? ErrorIds.First() : null;
-        }
-
-        foreach (int errorId in mutuallyExclusiveErrorIds)
-        {
-            if (ErrorIds.Contains(errorId))
-            {
-                return errorId;
-            }
-        }
-
-        return null;
-    }
-
-    public Error? GetErrorIfTriggered(params int[] mutuallyExclusiveErrorIds)
-    {
-        int? currentErrorId = GetErrorIdIfTriggered(mutuallyExclusiveErrorIds);
-        return currentErrorId != null ? GetError(currentErrorId.Value) : null;
-    }
+//    public Error? GetErrorIfTriggered(params int[] mutuallyExclusiveErrorIds)
+//    {
+//        int? currentErrorId = GetErrorIdIfTriggered(mutuallyExclusiveErrorIds);
+//        return currentErrorId != null ? GetError(currentErrorId.Value) : null;
+//    }
 }
