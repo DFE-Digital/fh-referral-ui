1336f4c3da36a0eaba95862a0c3fa1e6f811f437
Phil Davies <github@phildavies.co.uk>
2023-06-27 12:03:23 +0100

    wip

---
+++ src/FamilyHubs.Referral.Web/Errors/ErrorState.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Immutable;
+using System.Diagnostics.CodeAnalysis;
+
+namespace FamilyHubs.Referral.Web.Errors;
+
+//todo: combine error handling from base class into this too?
+public class ErrorState : IErrorSummary
+{
+    private readonly ImmutableDictionary<int, Error> _possibleErrors;
+
+    public ErrorState(ImmutableDictionary<int, Error> possibleErrors, IEnumerable<int> triggeredErrors)
+    {
+        _possibleErrors = possibleErrors;
+        ErrorIds = triggeredErrors;
+    }
+
+    public bool HasErrors => ErrorIds.Any();
+
+    public IEnumerable<int> ErrorIds { get; }
+
+    public Error GetError(int errorId)
+    {
+        return _possibleErrors[errorId];
+    }
+
+    public bool HasError(params int[] errorIds)
+    {
+        return GetErrorIdIfTriggered(errorIds) != null;
+    }
+
+    [SuppressMessage("Minor Code Smell", "S3267:Loops should be simplified with \"LINQ\" expressions", Justification = "LINQ expression version is less simple")]
+    public int? GetErrorIdIfTriggered(params int[] mutuallyExclusiveErrorIds)
+    {
+        if (!mutuallyExclusiveErrorIds.Any())
+        {
+            return ErrorIds.Any() ? ErrorIds.First() : null;
+        }
+
+        foreach (int errorId in mutuallyExclusiveErrorIds)
+        {
+            if (ErrorIds.Contains(errorId))
+            {
+                return errorId;
+            }
+        }
+
+        return null;
+    }
+
+    public Error? GetErrorIfTriggered(params int[] mutuallyExclusiveErrorIds)
+    {
+        int? currentErrorId = GetErrorIdIfTriggered(mutuallyExclusiveErrorIds);
+        return currentErrorId != null ? GetError(currentErrorId.Value) : null;
+    }
+}
--- src/FamilyHubs.Referral.Web/Pages/ProfessionalReferral/ContactByPhone.cshtml.cs
+++ src/FamilyHubs.Referral.Web/Pages/ProfessionalReferral/ContactByPhone.cshtml.cs
@@ -1,6 +1,5 @@
 using System.Collections.Immutable;
 using System.ComponentModel.DataAnnotations;
-using System.Diagnostics.CodeAnalysis;
 using FamilyHubs.Referral.Core.DistributedCache;
 using FamilyHubs.Referral.Core.Models;
 using FamilyHubs.Referral.Core.ValidationAttributes;
@@ -10,58 +9,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace FamilyHubs.Referral.Web.Pages.ProfessionalReferral;
 
-//todo: combine error handling from base class into this too?
-public class ErrorState : IErrorSummary
-{
-    private readonly ImmutableDictionary<int, Error> _possibleErrors;
-
-    public ErrorState(ImmutableDictionary<int, Error> possibleErrors, IEnumerable<int> triggeredErrors)
-    {
-        _possibleErrors = possibleErrors;
-        ErrorIds = triggeredErrors;
-    }
-
-    public bool HasErrors => ErrorIds.Any();
-
-    public IEnumerable<int> ErrorIds { get; }
-
-    public Error GetError(int errorId)
-    {
-        return _possibleErrors[errorId];
-    }
-
-    public bool HasError(params int[] errorIds)
-    {
-        return GetErrorIdIfTriggered(errorIds) != null;
-    }
-
-    [SuppressMessage("Minor Code Smell", "S3267:Loops should be simplified with \"LINQ\" expressions", Justification = "LINQ expression version is less simple")]
-    public int? GetErrorIdIfTriggered(params int[] mutuallyExclusiveErrorIds)
-    {
-        if (!mutuallyExclusiveErrorIds.Any())
-        {
-            return ErrorIds.Any() ? ErrorIds.First() : null;
-        }
-
-        foreach (int errorId in mutuallyExclusiveErrorIds)
-        {
-            if (ErrorIds.Contains(errorId))
-            {
-                return errorId;
-            }
-        }
-
-        return null;
-    }
-
-    public Error? GetErrorIfTriggered(params int[] mutuallyExclusiveErrorIds)
-    {
-        int? currentErrorId = GetErrorIdIfTriggered(mutuallyExclusiveErrorIds);
-        return currentErrorId != null ? GetError(currentErrorId.Value) : null;
-    }
-}
-
-public class ContactByPhoneModel : ProfessionalReferralCacheModel//, IErrorSummary
+public class ContactByPhoneModel : ProfessionalReferralCacheModel
 {
     // one of these for all errors?
     private static readonly ImmutableDictionary<int, Error> PossibleErrors = ImmutableDictionary
@@ -126,74 +74,4 @@ public class ContactByPhoneModel : ProfessionalReferralCacheModel//, IErrorSumma
 
         return NextPage();
     }
-
-//    //todo: work these into base
-//    public IEnumerable<int> ErrorIds
-//    {
-//        get
-//        {
-//            return ConnectionRequestModel?.ErrorState?.Errors.Select(e => (int) e) ?? Enumerable.Empty<int>();
-//        }
-//    }
-
-//    public Error GetError(int errorId)
-//    {
-//        //todo: have a static immutable dictionary
-//        return (ProfessionalReferralError) errorId switch
-//        {
-//            ProfessionalReferralError.ContactByPhone_NoContactSelected
-//                => new Error((int)ProfessionalReferralError.ContactByPhone_NoContactSelected, "email", "Select how the service can contact you"),
-//            ProfessionalReferralError.ContactByPhone_NoTelephoneNumber
-//                => new Error((int)ProfessionalReferralError.ContactByPhone_NoTelephoneNumber, "contact-by-phone", "Enter a UK telephone number"),
-//            ProfessionalReferralError.ContactByPhone_InvalidTelephoneNumber
-//                => new Error((int)ProfessionalReferralError.ContactByPhone_InvalidTelephoneNumber, "contact-by-phone", "Enter a telephone number, like 01632 960 001, 07700 900 982 or +44 808 157 0192"),
-//            _ => throw new NotImplementedException()
-//        };
-//    }
-
-//    //public bool HasError(int errorId)
-//    //{
-//    //    return ErrorIds.Contains(errorId);
-//    //}
-
-//    public bool HasError(params int[] errorIds)
-//    {
-//        return GetErrorIdIfTriggered(errorIds) != null;
-////#pragma warning disable S3267 // Loops should be simplified with "LINQ" expressions
-////        foreach (int errorId in errorIds)
-////        {
-////            if (ErrorIds.Contains(errorId))
-////            {
-////                return true;
-////            }
-////        }
-////#pragma warning restore S3267 // Loops should be simplified with "LINQ" expressions
-
-////        return false;
-//    }
-
-//    [SuppressMessage("Minor Code Smell", "S3267:Loops should be simplified with \"LINQ\" expressions", Justification = "LINQ expression version is less simple")]
-//    public int? GetErrorIdIfTriggered(params int[] mutuallyExclusiveErrorIds)
-//    {
-//        if (!mutuallyExclusiveErrorIds.Any())
-//        {
-//            return ErrorIds.Any() ? ErrorIds.First() : null;
-//        }
-
-//        foreach (int errorId in mutuallyExclusiveErrorIds)
-//        {
-//            if (ErrorIds.Contains(errorId))
-//            {
-//                return errorId;
-//            }
-//        }
-
-//        return null;
-//    }
-
-//    public Error? GetErrorIfTriggered(params int[] mutuallyExclusiveErrorIds)
-//    {
-//        int? currentErrorId = GetErrorIdIfTriggered(mutuallyExclusiveErrorIds);
-//        return currentErrorId != null ? GetError(currentErrorId.Value) : null;
-//    }
 }
