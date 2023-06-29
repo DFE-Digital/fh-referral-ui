15b0cb60ed571c98919afa8b29dbd351919622a2
Phil Davies <github@phildavies.co.uk>
2023-06-29 10:49:31 +0100

    wip

---
+++ src/FamilyHubs.Referral.Web/Errors/ErrorDictionaryExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Immutable;
+
+namespace FamilyHubs.Referral.Web.Errors;
+
+public static class ErrorDictionaryExtensions
+{
+    public static ImmutableDictionary<int, Error> Add<T>(
+        this ImmutableDictionary<int, Error> dictionary,
+        T errorId,
+        string htmlElementId,
+        string errorMessage)
+        where T : struct, Enum, IConvertible
+    {
+        return dictionary.Add((int)(IConvertible)errorId, new Error((int)(IConvertible)errorId, htmlElementId, errorMessage));
+    }
+}
