3727b941e9d1542d742d405abf24124555e4e046
Phil Davies <github@phildavies.co.uk>
2023-07-12 10:20:19 +0100

    clear down cache when user signs out

---
+++ src/FamilyHubs.Referral.Web/Pages/sign-out/Index.cshtml.cs
@@ -0,0 +1,23 @@
+using FamilyHubs.Referral.Core.DistributedCache;
+using FamilyHubs.SharedKernel.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FamilyHubs.Referral.Web.Pages.sign_out;
+
+public class IndexModel : PageModel
+{
+    private readonly IConnectionRequestDistributedCache _connectionRequestCache;
+
+    public IndexModel(IConnectionRequestDistributedCache connectionRequestCache)
+    {
+        _connectionRequestCache = connectionRequestCache;
+    }
+
+    public async Task<IActionResult> OnGetAsync(int requestNumber)
+    {
+        var professionalUser = HttpContext.GetFamilyHubsUser();
+        await _connectionRequestCache.RemoveAsync(professionalUser.Email);
+        return Redirect("/account/signout");
+    }
+}
