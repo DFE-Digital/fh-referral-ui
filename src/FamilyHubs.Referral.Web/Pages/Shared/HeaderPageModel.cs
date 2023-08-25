fe77f7f643288b7cffe96afc80841d941a078bc9
Phil Davies <github@phildavies.co.uk>
2023-08-25 10:12:20 +0100

    wip

--- src/FamilyHubs.Referral.Web/Pages/Shared/HeaderPageModel.cs
+++ src/FamilyHubs.Referral.Web/Pages/Shared/HeaderPageModel.cs
@@ -1,4 +1,6 @@
-﻿using FamilyHubs.SharedKernel.Razor.FamilyHubsUi.Delegators;
+﻿using FamilyHubs.SharedKernel.Identity;
+using FamilyHubs.SharedKernel.Identity.Models;
+using FamilyHubs.SharedKernel.Razor.FamilyHubsUi.Delegators;
 using FamilyHubs.SharedKernel.Razor.FamilyHubsUi.Options;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -6,11 +8,29 @@ namespace FamilyHubs.Referral.Web.Pages.Shared;
 
 public class HeaderPageModel : PageModel, IFamilyHubsHeader
 {
+    //too early...
+    protected FamilyHubsUser FamilyHubsUser { get; }
+
+    public HeaderPageModel()
+    {
+        //todo: reuse in derived classes
+        FamilyHubsUser = HttpContext.GetFamilyHubsUser();
+    }
+
     public bool ShowActionLinks => User.Identity?.IsAuthenticated == true;
     public bool ShowNavigationMenu => true;
 
     LinkStatus IFamilyHubsHeader.GetStatus(FhLinkOptions link)
     {
+        //todo: no active if home page
         return link.Text == "Search for service" ? LinkStatus.Active : LinkStatus.Visible;
+    }
+
+    IEnumerable<FhLinkOptions> IFamilyHubsHeader.NavigationLinks(FhLinkOptions[] navigationLinks)
+    {
+        string role = HttpContext.GetRole();
+        return role is RoleTypes.VcsProfessional or RoleTypes.VcsDualRole
+               ? navigationLinks.Reverse()
+               : navigationLinks;
     }
 }
