3919727f4f00a2defa9f2e7258041f2cb659087b
Phil Davies <github@phildavies.co.uk>
2023-08-30 12:19:19 +0100

    fix start now button on homepage when using path based routing with a /start sub site trigger path

--- src/FamilyHubs.Referral.Web/Pages/pro-redirect/Index.cshtml.cs
+++ src/FamilyHubs.Referral.Web/Pages/pro-redirect/Index.cshtml.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Options;
 
-namespace FamilyHubs.Referral.Web.Pages.start_now;
+namespace FamilyHubs.Referral.Web.Pages.pro_redirect;
 
 [Authorize]
 public class IndexModel : PageModel
