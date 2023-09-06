861b4053c9ae80a4d8ebf39aaf1181939ecb7037
Phil Davies <github@phildavies.co.uk>
2023-09-06 14:56:36 +0100

    fix typescript transpilation + update typescript version + back button component wip

--- src/FamilyHubs.Referral.Web/scripts/app.ts
+++ src/FamilyHubs.Referral.Web/scripts/app.ts
@@ -30,4 +30,28 @@ window.fhgov = new fhgov();
 
 document.addEventListener('DOMContentLoaded', function () {
     window.fhgov.init();
+    initializeBackButtons();
 });
+
+//todo: needs to live in the fh packages
+
+function initializeBackButtons() {
+
+    // Check if the page wasn't opened in a new tab or a standalone window
+    if (window.opener === null || typeof window.opener === "undefined") {
+
+        // Select all elements with the data-model attribute set to "fh-back-button"
+        const backButtonElements = document.querySelectorAll('[data-model="fh-back-button"]');
+
+        // Loop through each matching element and apply the logic
+        backButtonElements.forEach((backButton: HTMLElement) => {
+            backButton.style.display = "block";
+
+            // Add an event listener to handle the back button click
+            backButton.addEventListener("click", () => {
+                // Go back to the previous page in the browser's history
+                window.history.back();
+            });
+        });
+    }
+}
