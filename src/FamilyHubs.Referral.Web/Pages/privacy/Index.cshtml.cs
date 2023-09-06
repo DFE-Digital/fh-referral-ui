223770d892612f5e489281e46671d9e3f1200825
Phil Davies <github@phildavies.co.uk>
2023-09-06 10:44:37 +0100

    add terms of use and privacy page wip + add privacy footer

---
+++ src/FamilyHubs.Referral.Web/Pages/privacy/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FamilyHubs.Referral.Web.Pages.privacy
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ src/FamilyHubs.Referral.Web/Pages/terms-of-use/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FamilyHubs.Referral.Web.Pages.terms_of_use
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
