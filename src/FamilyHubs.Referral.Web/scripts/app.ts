5c0fdfdcb00d982930af5ef51ae40f445dd3dccc
Phil Davies <github@phildavies.co.uk>
2023-09-07 10:11:51 +0100

    remove app back button

--- src/FamilyHubs.Referral.Web/scripts/app.ts
+++ src/FamilyHubs.Referral.Web/scripts/app.ts
@@ -30,28 +30,4 @@ window.fhgov = new fhgov();
 
 document.addEventListener('DOMContentLoaded', function () {
     window.fhgov.init();
-    initializeBackButtons();
 });
-
-//todo: needs to live in the fh packages
-
-function initializeBackButtons() {
-
-    // Check if the page wasn't opened in a new tab or a standalone window
-    if (history.length > 1) {
-
-        // Select all elements with the data-model attribute set to "fh-back-button"
-        const backButtonElements = document.querySelectorAll('[data-model="fh-back-button"]');
-
-        // Loop through each matching element and apply the logic
-        backButtonElements.forEach((backButton: HTMLElement) => {
-            backButton.style.display = "block";
-
-            // Add an event listener to handle the back button click
-            backButton.addEventListener("click", () => {
-                // Go back to the previous page in the browser's history
-                window.history.back();
-            });
-        });
-    }
-}
