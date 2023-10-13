1ccdcf096165235abe46e664729f4a23b39dc4b4
Phil Davies <github@phildavies.co.uk>
2023-10-13 11:13:33 +0100

    health wip

---
+++ src/FamilyHubs.Referral.Infrastructure/Health/HealthCheck.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using HealthChecks.UI.Client;
+
+namespace FamilyHubs.Referral.Infrastructure.Health;
+
+public static class HealthCheck
+{
+    // extension method to add health checks
+    public static IServiceCollection AddSiteHealthChecks(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        var serviceDirectoryApiUrl = configuration.GetValue<string>("ServiceDirectoryUrl");
+        var referralApiUrl = configuration.GetValue<string>("ReferralApiUrl");
+        var notificationApiUrl = configuration.GetValue<string>("Notification:Endpoint");
+        var idamsApiUrl = configuration.GetValue<string>("Idams:Endpoint");
+        //todo: postcodes io url is hardcoded! switch to find's postcodes io client
+#pragma warning disable S1075
+        const string postcodesIoUrl = "http://api.postcodes.io";
+#pragma warning restore S1075
+        var sqlServerCacheConnectionString = configuration.GetValue<string>("SqlServerCache:Connection");
+
+        //todo: null handling. use config exception?
+
+        // we handle API failures as Degraded, so that App Services doesn't remove or replace the instance (all instances!) due to an API being down
+        services.AddHealthChecks()
+            .AddUrlGroup(new Uri(postcodesIoUrl), "PostcodesIo", HealthStatus.Degraded, new[] { "ExternalAPI" })
+            .AddUrlGroup(new Uri(serviceDirectoryApiUrl!), "ServiceDirectoryAPI", HealthStatus.Degraded, new[] { "InternalAPI" })
+            .AddUrlGroup(new Uri(referralApiUrl!), "ReferralAPI", HealthStatus.Degraded, new[] { "InternalAPI" })
+            .AddUrlGroup(new Uri(notificationApiUrl!), "NotificationAPI", HealthStatus.Degraded, new[] { "InternalAPI" })
+            .AddUrlGroup(new Uri(idamsApiUrl!), "IdamsAPI", HealthStatus.Degraded, new[] { "InternalAPI" })
+            .AddSqlServer(sqlServerCacheConnectionString!, failureStatus: HealthStatus.Degraded, tags: new[] { "Database" });
+        //todo: check for one login, if we can
+        //todo: check feedback link?
+
+#pragma warning disable S125
+        // health check UI
+        // services
+        //     .AddHealthChecksUI()
+        //    .AddInMemoryStorage();
+
+        // services.AddInMemoryStorage();
+        // .AddInMemoryStorage();
+#pragma warning restore S125
+
+        return services;
+    }
+
+    public static WebApplication MapSiteHealthChecks(this WebApplication app)
+    {
+        app.MapHealthChecks("/health", new HealthCheckOptions
+        {
+            Predicate = _ => true,
+            ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+        });
+
+        return app;
+    }
+}
--- src/FamilyHubs.Referral.Web/StartupExtensions.cs
+++ src/FamilyHubs.Referral.Web/StartupExtensions.cs
@@ -11,12 +11,10 @@ using System.Diagnostics.CodeAnalysis;
 using FamilyHubs.Notification.Api.Client.Extensions;
 using FamilyHubs.Notification.Api.Client.Templates;
 using FamilyHubs.Referral.Core;
+using FamilyHubs.Referral.Infrastructure.Health;
 using FamilyHubs.Referral.Infrastructure.Notifications;
 using FamilyHubs.SharedKernel.DataProtection;
 using FamilyHubs.SharedKernel.Telemetry;
-using Microsoft.Extensions.Diagnostics.HealthChecks;
-using HealthChecks.UI.Client;
-using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 
 namespace FamilyHubs.Referral.Web;
 
@@ -60,38 +58,7 @@ public static class StartupExtensions
                 options.HtmlHelperOptions.ClientValidationEnabled = false;
             });
 
-        //todo: move this out to its own extension method
-        var serviceDirectoryApiUrl = configuration.GetValue<string>("ServiceDirectoryUrl");
-        var referralApiUrl = configuration.GetValue<string>("ReferralApiUrl");
-        var notificationApiUrl = configuration.GetValue<string>("Notification:Endpoint");
-        var idamsApiUrl = configuration.GetValue<string>("Idams:Endpoint");
-        //todo: postcodes io url is hardcoded! switch to find's postcodes io client
-#pragma warning disable S1075
-        const string postcodesIoUrl = "http://api.postcodes.io";
-#pragma warning restore S1075
-        var sqlServerCacheConnectionString = configuration.GetValue<string>("SqlServerCache:Connection");
-
-        //todo: null handling. use config exception?
-
-        // we handle API failures as Degraded, so that App Services doesn't remove or replace the instance (all instances!) due to an API being down
-        services.AddHealthChecks()
-            .AddUrlGroup(new Uri(postcodesIoUrl), "PostcodesIo", HealthStatus.Degraded, new[] {"ExternalAPI"})
-            .AddUrlGroup(new Uri(serviceDirectoryApiUrl!), "ServiceDirectoryAPI", HealthStatus.Degraded, new[] {"InternalAPI"})
-            .AddUrlGroup(new Uri(referralApiUrl!), "ReferralAPI", HealthStatus.Degraded, new[] {"InternalAPI"})
-            .AddUrlGroup(new Uri(notificationApiUrl!), "NotificationAPI", HealthStatus.Degraded, new[] {"InternalAPI"})
-            .AddUrlGroup(new Uri(idamsApiUrl!), "IdamsAPI", HealthStatus.Degraded, new[] {"InternalAPI"})
-            .AddSqlServer(sqlServerCacheConnectionString!, failureStatus: HealthStatus.Degraded, tags: new[] {"Database"});
-        //todo: check sql health too, as we have a direct dependency on it
-        //todo: check for one login, if we can
-        //todo: check feedback link?
-
-        // health check UI
-        //services
-        //    .AddHealthChecksUI()
-        //    .AddInMemoryStorage();
-
-        //services.AddInMemoryStorage();
-        //.AddInMemoryStorage();
+        services.AddSiteHealthChecks(configuration);
 
         services.AddFamilyHubs(configuration);
     }
@@ -197,11 +164,7 @@ public static class StartupExtensions
 
         app.MapRazorPages();
 
-        app.MapHealthChecks("/health", new HealthCheckOptions
-        {
-            Predicate = _ => true,
-            ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
-        });
+        app.MapSiteHealthChecks();
 
         return app.Services;
     }
