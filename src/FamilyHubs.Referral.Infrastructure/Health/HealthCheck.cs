dfeb2820f2d2cbf676efbb09581cd483b9561631
Phil Davies <github@phildavies.co.uk>
2023-10-13 15:01:44 +0100

    use /api/info endpoint for internal apis

--- src/FamilyHubs.Referral.Infrastructure/Health/HealthCheck.cs
+++ src/FamilyHubs.Referral.Infrastructure/Health/HealthCheck.cs
@@ -35,6 +35,12 @@ public static class HealthCheck
         // continue to throw it's own relevant exception
         if (!string.IsNullOrEmpty(apiUrl))
         {
+            if (urlType == UrlType.InternalApi)
+            {
+                //todo: add "/health" endpoints to all APIs
+                apiUrl += "/api/info";
+            }
+
             // we handle API failures as Degraded, so that App Services doesn't remove or replace the instance (all instances!) due to an API being down
             builder.AddUrlGroup(new Uri(apiUrl), name, HealthStatus.Degraded,
                 new[] {urlType.ToString()});
@@ -45,16 +51,12 @@ public static class HealthCheck
 
     public static IServiceCollection AddSiteHealthChecks(
         this IServiceCollection services,
-        IConfiguration configuration)
+        IConfiguration config)
     {
-        //todo: postcodes io url is hardcoded! switch to find's postcodes io client? strategic switch coming
-#pragma warning disable S1075
-        const string postcodesIoUrl = "http://api.postcodes.io";
-#pragma warning restore S1075
-        var oneLoginUrl = configuration.GetValue<string>("GovUkOidcConfiguration:Oidc:BaseUrl");
-        var sqlServerCacheConnectionString = configuration.GetValue<string>("SqlServerCache:Connection");
+        var oneLoginUrl = config.GetValue<string>("GovUkOidcConfiguration:Oidc:BaseUrl");
+        var sqlServerCacheConnectionString = config.GetValue<string>("SqlServerCache:Connection");
 
-        var keyVaultKey = configuration.GetValue<string>("DataProtection:KeyIdentifier");
+        var keyVaultKey = config.GetValue<string>("DataProtection:KeyIdentifier");
         int keysIndex = keyVaultKey!.IndexOf("/keys/");
         string keyVaultUrl = keyVaultKey[..keysIndex];
         string keyName = keyVaultKey[(keysIndex + 6)..];
@@ -66,26 +68,29 @@ public static class HealthCheck
         //todo: null handling. use config exception?
 
         TokenCredential keyVaultCredentials = new ClientSecretCredential(
-            configuration.GetValue<string>("DataProtection:TenantId"),
-            configuration.GetValue<string>("DataProtection:ClientId"),
-            configuration.GetValue<string>("DataProtection:ClientSecret"));
+            config.GetValue<string>("DataProtection:TenantId"),
+            config.GetValue<string>("DataProtection:ClientId"),
+            config.GetValue<string>("DataProtection:ClientSecret"));
 
         // we handle API failures as Degraded, so that App Services doesn't remove or replace the instance (all instances!) due to an API being down
         var healthCheckBuilder = services.AddHealthChecks()
             .AddIdentityServer(new Uri(oneLoginUrl!), name: "One Login", failureStatus: HealthStatus.Degraded, tags: new[] { "ExternalAPI" })
-            .AddUrlGroup(new Uri(postcodesIoUrl), "PostcodesIo", HealthStatus.Degraded, new[] { "ExternalAPI" })
+#pragma warning disable S1075
+            //todo: postcodes io url is hardcoded! switch to find's postcodes io client? strategic switch coming
+            .AddUrlGroup(new Uri("http://api.postcodes.io"), "PostcodesIo", HealthStatus.Degraded, new[] { "ExternalAPI" })
+#pragma warning restore S1075
             //todo: family hubs helper?
-            .AddApi("Feedback Site", "FamilyHubsUi:FeedbackUrl", configuration)
-            .AddApi("Service Directory API", "ServiceDirectoryUrl", configuration)
-            .AddApi("Referral API", "ReferralApiUrl", configuration)
-            .AddApi("Notification API", "Notification:Endpoint", configuration)
-            .AddApi("Idams API", "Idams:Endpoint", configuration)
+            .AddApi("Feedback Site", "FamilyHubsUi:FeedbackUrl", config)
+            .AddApi("Service Directory API", "ServiceDirectoryUrl", config)
+            .AddApi("Referral API", "ReferralApiUrl", config)
+            .AddApi("Notification API", "Notification:Endpoint", config)
+            .AddApi("Idams API", "Idams:Endpoint", config)
             .AddSqlServer(sqlServerCacheConnectionString!, failureStatus: HealthStatus.Degraded, tags: new[] { "Database" })
             //todo: tag as AKV, name as Data Protection Key?
-            .AddAzureKeyVault(new Uri(keyVaultUrl!), keyVaultCredentials, s => s.AddKey(keyName), name:"Azure Key Vault", failureStatus: HealthStatus.Degraded, tags: new[] { "Infrastructure" });
+            .AddAzureKeyVault(new Uri(keyVaultUrl), keyVaultCredentials, s => s.AddKey(keyName), name:"Azure Key Vault", failureStatus: HealthStatus.Degraded, tags: new[] { "Infrastructure" });
 
         // not usually set running locally
-        string? aiInstrumentationKey = configuration.GetValue<string>("APPINSIGHTS_INSTRUMENTATIONKEY");
+        string? aiInstrumentationKey = config.GetValue<string>("APPINSIGHTS_INSTRUMENTATIONKEY");
         if (!string.IsNullOrEmpty(aiInstrumentationKey))
         {
             //todo: check in dev env
