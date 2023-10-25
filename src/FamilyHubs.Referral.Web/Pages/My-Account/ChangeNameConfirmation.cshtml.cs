af21e01e774ff7c12345f9810658429e1212e6c8
Phil Davies <github@phildavies.co.uk>
2023-10-25 09:57:31 +0100

    my account wip

--- src/FamilyHubs.Referral.Web/Pages/My-Account/ChangeNameConfirmation.cshtml.cs
+++ src/FamilyHubs.Referral.Web/Pages/My-Account/ChangeNameConfirmation.cshtml.cs
@@ -1,9 +1,14 @@
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+using FamilyHubs.Referral.Web.Pages.Shared;
+using FamilyHubs.SharedKernel.Identity;
 
-namespace FamilyHubs.Referral.Web.Pages.My_Account
+namespace FamilyHubs.Referral.Web.Pages.My_Account;
+
+public class ChangeNameConfirmationModel : HeaderPageModel
 {
-    public class ChangeNameConfirmationModel : PageModel
+    public string? NewName { get; set; }
+
+    public void OnGet()
     {
+        NewName = HttpContext.GetFamilyHubsUser().FullName;
     }
 }
