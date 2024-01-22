4e3d2decfd55bf8db789e9750d396d0656ab0624
Phil Davies <github@phildavies.co.uk>
2024-01-22 11:44:29 +0000

    use display code from updated sd shared

--- src/FamilyHubs.Referral.Web/Pages/ProfessionalReferral/LocalOfferDetail.cshtml.cs
+++ src/FamilyHubs.Referral.Web/Pages/ProfessionalReferral/LocalOfferDetail.cshtml.cs
@@ -6,84 +6,8 @@ using System.Text.RegularExpressions;
 using Microsoft.AspNetCore.Authorization;
 using FamilyHubs.Referral.Web.Pages.Shared;
 using FamilyHubs.SharedKernel.Identity;
-using FamilyHubs.ServiceDirectory.Shared.Enums;
 
 namespace FamilyHubs.Referral.Web.Pages.ProfessionalReferral;
-
-//todo: this is going to be moved to service directory common for reuse
-public static class ServiceDisplayExtensions
-{
-    public static IEnumerable<string> GetServiceAvailability(this ServiceDto service)
-    {
-        return service.Schedules.GetServiceAvailability();
-    }
-
-    public static IEnumerable<string> GetServiceAvailability(this ICollection<ScheduleDto>? schedules)
-    {
-        if (schedules == null || schedules.Count == 0)
-            return Enumerable.Empty<string>();
-
-        var weekdaysAndWeekends = GetWeekdaysAndWeekends(schedules).ToArray();
-        var timeDescription = GetTimeDescription(schedules);
-
-        bool hasWeekdaysAndWeekends = weekdaysAndWeekends.Length > 0;
-
-        if (!hasWeekdaysAndWeekends && timeDescription == null)
-            return Enumerable.Empty<string>();
-
-        if (hasWeekdaysAndWeekends && timeDescription != null)
-        {
-            return weekdaysAndWeekends.Append("").Append(timeDescription);
-        }
-
-        return hasWeekdaysAndWeekends ? weekdaysAndWeekends : new[] { timeDescription! };
-    }
-
-    public static IEnumerable<string> GetWeekdaysAndWeekends(this ServiceDto service)
-    {
-        return service.Schedules.GetWeekdaysAndWeekends();
-    }
-
-    public static IEnumerable<string> GetWeekdaysAndWeekends(this ICollection<ScheduleDto> schedules)
-    {
-        return schedules
-            .Select(ScheduleDescription)
-            .Where(d => !string.IsNullOrEmpty(d))!;
-    }
-
-    private static string? ScheduleDescription(ScheduleDto schedule)
-    {
-        if (schedule.Freq != FrequencyType.Weekly)
-        {
-            return null;
-        }
-
-        string dayType;
-        switch (schedule.ByDay)
-        {
-            case "MO,TU,WE,TH,FR":
-                dayType = "Weekdays";
-                break;
-            case "SA,SU":
-                dayType = "Weekends";
-                break;
-            default:
-                return null;
-        }
-
-        return $"{dayType}: {schedule.OpensAt:h:mmtt} to {schedule.ClosesAt:h:mmtt}";
-    }
-
-    public static string? GetTimeDescription(this ServiceDto service)
-    {
-        return service.Schedules.GetTimeDescription();
-    }
-
-    public static string? GetTimeDescription(this ICollection<ScheduleDto> schedules)
-    {
-        return schedules.FirstOrDefault(x => x.Description != null)?.Description;
-    }
-}
 
 [Authorize(Roles = RoleGroups.LaOrVcsProfessionalOrDualRole)]
 public class LocalOfferDetailModel : HeaderPageModel
